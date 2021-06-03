4e24630289eba514be33633eaaf1b79ff50c3c4a
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2021-06-03 11:16:43 +0200

    fix: volume in the dropdown list

--- components/partials/derivatives/markets/derivative.vue
+++ components/partials/derivatives/markets/derivative.vue
@@ -61,7 +61,7 @@
 
 <script lang="ts">
 import Vue, { PropType } from 'vue'
-import { BigNumberInBase } from '@injectivelabs/utils'
+import { BigNumberInBase, BigNumberInWei } from '@injectivelabs/utils'
 import { ZERO_IN_BASE } from '~/app/utils/constants'
 import { UiDerivativeMarket, Icon, Change } from '~/types'
 
@@ -98,7 +98,12 @@ export default Vue.extend({
         return ZERO_IN_BASE
       }
 
-      return new BigNumberInBase(market.volume)
+      return new BigNumberInBase(
+        new BigNumberInWei(market.volume)
+          .toBase(market.quoteToken.decimals)
+          .dp(0)
+          .toFixed()
+      )
     },
 
     change(): BigNumberInBase {
