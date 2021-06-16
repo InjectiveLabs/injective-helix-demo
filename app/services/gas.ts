2d830ccc6e58546b95460b0d8cacb4c339fb8ecb
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2021-06-16 08:57:38 +0200

    fix: catch block for gasstation

--- app/services/gas.ts
+++ app/services/gas.ts
@@ -4,17 +4,22 @@ import { EthGasStationResult } from '~/types'
 
 export const fetchGasPrice = async (): Promise<string> => {
   const client = new HttpClient('https://ethgasstation.info/json')
-  const response = (await client.get(`ethgasAPI.json`)) as {
-    data: EthGasStationResult
-  }
 
-  if (!response || (response && !response.data)) {
+  try {
+    const response = (await client.get(`ethgasAPI.json`)) as {
+      data: EthGasStationResult
+    }
+
+    if (!response || (response && !response.data)) {
+      return TESTNET_GAS_PRICE.toString()
+    }
+
+    return new BigNumberInWei(
+      new BigNumber(response.data.average / 10).multipliedBy(GWEI_IN_WEI)
+    )
+      .toBase()
+      .toString()
+  } catch (e) {
     return TESTNET_GAS_PRICE.toString()
   }
-
-  return new BigNumberInWei(
-    new BigNumber(response.data.average / 10).multipliedBy(GWEI_IN_WEI)
-  )
-    .toBase()
-    .toString()
 }
