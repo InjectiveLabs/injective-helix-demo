60b48beee3e89dba18b33a7aafffccf5fa77f469
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2021-06-28 23:04:47 +0200

    fix: toast on error even in dev

--- plugins/utils.ts
+++ plugins/utils.ts
@@ -4,9 +4,9 @@ import { IS_PRODUCTION } from '~/app/utils/constants'
 
 export default ({ app }: Context, inject: any) => {
   inject('onRejected', (error: Error) => {
+    app.$toast.error(error.message)
     if (IS_PRODUCTION) {
       app.$bugsnag.notify(error)
-      app.$toast.error(error.message)
     } else {
       console.error(error)
     }
