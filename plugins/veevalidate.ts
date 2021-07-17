ffcb20c405eee148bf1c9a326c8064f9ab9692c4
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2021-07-17 12:16:20 +0200

    chore: copy change - resolves #127

--- plugins/veevalidate.ts
+++ plugins/veevalidate.ts
@@ -8,7 +8,12 @@ localize({
       required: 'This field is required',
       positiveNumber: 'This field is not a valid number',
       email: 'This field should be a valid email',
-      between: 'This field should be between {min} and {max}',
+      between: (_field: string, params: Record<string, any>) =>
+        `${
+          params.max <= params.min
+            ? `Your input value of ${params._value_} cant be higher than ${params.max}`
+            : `This field should be between ${params.min} and ${params.max}`
+        }`,
       max: 'This field should be less than {length}',
       min: 'This field should be greater than {length}'
     }
