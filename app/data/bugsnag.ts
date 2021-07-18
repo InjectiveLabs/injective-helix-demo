bc77d5f9c147114c3df529779f0f76eb229e002b
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2021-07-18 02:58:51 +0200

    fix: generic error messages not reported - resolves #133

---
+++ app/data/bugsnag.ts
@@ -0,0 +1,5 @@
+export const excludedMessagesFromReporting = [
+  'Metamask is not installed.',
+  'MetaMask Tx Signature: User denied transaction signature.',
+  'Please update your Chrome Browser'
+]
--- plugins/utils.ts
+++ plugins/utils.ts
@@ -1,11 +1,15 @@
 /* eslint-disable no-console */
 import { Context } from '@nuxt/types'
+import { excludedMessagesFromReporting } from '~/app/data/bugsnag'
 import { IS_PRODUCTION } from '~/app/utils/constants'
 
 export default ({ app }: Context, inject: any) => {
   inject('onRejected', (error: Error) => {
     app.$toast.error(error.message)
-    if (IS_PRODUCTION) {
+    if (
+      IS_PRODUCTION &&
+      !excludedMessagesFromReporting.includes(error.message)
+    ) {
       app.$bugsnag.notify(error)
     } else {
       console.error(error)
