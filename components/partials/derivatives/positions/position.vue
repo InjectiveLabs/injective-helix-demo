1cc91dc89e110271e129eee1f722b61ce0a25395
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2021-07-27 21:43:29 +0200

    feat: added pnl profit in asset - resolves #159

--- components/partials/derivatives/positions/position.vue
+++ components/partials/derivatives/positions/position.vue
@@ -47,21 +47,34 @@
         }"
       />
     </td>
-    <td is="v-ui-table-td" xs center>
-      <div v-if="!pnl.isNaN()" class="flex items-center justify-center">
-        <span
-          class="mr-1"
-          :class="pnl.gte(0) ? 'text-primary-500' : 'text-accent-500'"
-          >≈</span
-        >
-        <v-ui-format-percent
-          v-bind="{
-            appendPlusSign: true,
-            precision: 2,
-            value: pnl.toString(),
-            class: pnl.gte(0) ? 'text-primary-500' : 'text-accent-500'
-          }"
-        />
+    <td is="v-ui-table-td" center>
+      <div
+        v-if="!pnl.isNaN()"
+        class="flex items-center justify-center text-2xs"
+        :class="pnlClass"
+      >
+        <span class="mr-1">≈</span>
+        <div class="flex items-center">
+          <span class="mr-1 flex items-center">
+            <v-ui-format-price
+              v-bind="{
+                value: notionalPnl,
+                class: pnlClass,
+                decimals: market.priceDecimals
+              }"
+              class="text-right block"
+            /><span class="ml-1">{{ market.quoteToken.symbol }}</span></span
+          >
+          <span class="flex" style="margin-top: -2px"
+            >(<v-ui-format-percent
+              v-bind="{
+                appendPlusSign: true,
+                precision: 2,
+                value: pnl.toString()
+              }"
+            />)</span
+          >
+        </div>
       </div>
       <v-ui-text v-else muted>{{ $t('not_available_n_a') }}</v-ui-text>
     </td>
@@ -238,7 +251,6 @@ export default Vue.extend({
       const [buy] = buys
       const highestBuy = new BigNumberInBase(buy ? buy.price : 0)
       const lowestSell = new BigNumberInBase(sell ? sell.price : 0)
-      const isPositionLong = position.direction === TradeDirection.Long
       const executionPrice = new BigNumberInBase(
         highestBuy.plus(lowestSell).div(2)
       )
@@ -249,13 +261,33 @@ export default Vue.extend({
             market.quoteToken.decimals
           )
         )
-        .times(isPositionLong ? 1 : -1)
+        .times(position.direction === TradeDirection.Long ? 1 : -1)
+    },
+
+    notionalPnl(): BigNumberInBase {
+      const { pnl, market, position } = this
+
+      if (!market) {
+        return ZERO_IN_BASE
+      }
+
+      return new BigNumberInBase(
+        new BigNumberInWei(pnl.dividedBy(100).times(position.margin)).toBase(
+          market.quoteToken.decimals
+        )
+      )
+    },
+
+    pnlClass(): string {
+      const { pnl } = this
+
+      return pnl.gte(0) ? 'text-primary-500' : 'text-accent-500'
     },
 
     effectiveLeverage(): BigNumberInBase {
       const { notionalValue, margin, market, pnl } = this
 
-      if (margin.lte(0) || notionalValue.lte(0) || !market || !pnl) {
+      if (margin.lte(0) || notionalValue.lte(0) || !market || pnl.isNaN()) {
         return ZERO_IN_BASE
       }
 
