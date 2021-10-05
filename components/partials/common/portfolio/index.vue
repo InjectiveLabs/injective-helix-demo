372442d121b226a01e6c1e4d623794da289a333c
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2021-10-05 13:19:13 +0200

    refactor: part 26

--- components/partials/common/portfolio/index.vue
+++ components/partials/common/portfolio/index.vue
@@ -5,11 +5,11 @@
     </h3>
     <v-card class="mt-6">
       <div class="flex flex-wrap -mx-4">
-        <div class="w-full lg:w-2/5 px-4">
+        <div class="w-full lg:w-1/4 px-4">
           <v-overview />
         </div>
-        <div class="w-full lg:w-3/5 px-4">
-          <v-chart />
+        <div class="w-full lg:w-3/4 px-4">
+          <v-stats />
         </div>
       </div>
     </v-card>
@@ -19,12 +19,12 @@
 <script lang="ts">
 import Vue from 'vue'
 import VOverview from './overview.vue'
-import VChart from './chart.vue'
+import VStats from './stats.vue'
 
 export default Vue.extend({
   components: {
     VOverview,
-    VChart
+    VStats
   }
 })
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,17 @@
 <template>
   <div class="h-full w-full flex flex-wrap py-4">
     <div class="container">
-      <div class="text-center w-full lg:w-2/3 mx-auto">
-        <h1 class="font-black text-white text-3xl uppercase tracking-wide">
-          <img
-            src="/images/home-main.svg?v5"
-            alt="Injective Exchange"
-            class="h-48 mx-auto w-auto"
-          />
-        </h1>
-        <h3 class="text-primary-500 uppercase tracking-wider font-black">
-          {{ $t('welcome_to_ip') }}
-        </h3>
-        <h4 class="text-gray-300 text-lg mt-2">
-          {{ $t('welcome_to_ip_sub') }}
-        </h4>
-      </div>
-
-      <div class="mt-12 w-full mx-auto lg:w-3/4">
-        <v-cards />
+      <div class="w-full mx-auto lg:w-3/4">
+        <div class="text-center w-full mt-12">
+          <div class="flex items-center justify-center">
+            <v-logo class="h-12 w-auto mr-2" alt="Injective" />
+            <v-logo-text class="h-12 w-auto" />
+          </div>
+          <h3 class="text-primary-500 uppercase tracking-widest font-bold mt-6">
+            {{ $t('welcome_to_ip') }}
+          </h3>
+        </div>
+        <v-cards class="mt-12" />
         <v-portfolio class="mt-12" />
         <v-panel :title="$t('derivatives')" class="mt-12">
           <v-derivatives />
@@ -37,13 +30,17 @@ import VCards from '~/components/partials/home/cards.vue'
 import VPortfolio from '~/components/partials/common/portfolio/index.vue'
 import VSpot from '~/components/partials/common/markets/spot.vue'
 import VDerivatives from '~/components/partials/common/markets/derivatives.vue'
+import VLogo from '~/components/elements/logo.vue'
+import VLogoText from '~/components/elements/logo-text.vue'
 
 export default Vue.extend({
   components: {
     VSpot,
     VCards,
     VPortfolio,
-    VDerivatives
+    VDerivatives,
+    VLogoText,
+    VLogo
   }
 })
 </script>
