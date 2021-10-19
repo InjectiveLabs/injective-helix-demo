7ea08e0de4ce1bda6bfbef2b7f377a7f05e01692
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2021-10-19 19:16:56 +0200

    fix: gas station

--- app/services/gas.ts
+++ app/services/gas.ts
@@ -1,13 +1,34 @@
 import { HttpClient, BigNumber, BigNumberInWei } from '@injectivelabs/utils'
 import { ChainId } from '@injectivelabs/ts-types'
-import { IS_TESTNET, GWEI_IN_WEI, DEFAULT_GAS_PRICE, CHAIN_ID } from '../utils/constants'
-import { EthGasStationResult } from '~/types'
+import {
+  IS_TESTNET,
+  GWEI_IN_WEI,
+  DEFAULT_GAS_PRICE,
+  CHAIN_ID
+} from '../utils/constants'
+import { EtherchainResult, EthGasStationResult } from '~/types'
 
-export const fetchGasPrice = async (): Promise<string> => {
-  if (IS_TESTNET || CHAIN_ID === ChainId.Kovan) {
-    return new BigNumberInWei(DEFAULT_GAS_PRICE).toString()
+export const fetchGasPriceFromEtherchain = async (): Promise<string> => {
+  try {
+    const response = (await new HttpClient(
+      'https://www.etherchain.org/api/'
+    ).get(`gasPriceOracle`)) as {
+      data: EtherchainResult
+    }
+
+    if (!response || (response && !response.data)) {
+      throw new Error('No response from Etherchain')
+    }
+
+    return new BigNumberInWei(
+      new BigNumber(response.data.fast * 10).multipliedBy(GWEI_IN_WEI)
+    ).toString()
+  } catch (e: any) {
+    throw new Error(e.message)
   }
+}
 
+export const fetchGasPriceFromEthGasStation = async (): Promise<string> => {
   try {
     const response = (await new HttpClient(
       'https://ethgasstation.info/json'
@@ -16,13 +37,33 @@ export const fetchGasPrice = async (): Promise<string> => {
     }
 
     if (!response || (response && !response.data)) {
-      return new BigNumberInWei(DEFAULT_GAS_PRICE).toString()
+      throw new Error('No response from Ethgasstation')
     }
 
     return new BigNumberInWei(
       new BigNumber(response.data.fast / 10).multipliedBy(GWEI_IN_WEI)
     ).toString()
-  } catch (e) {
-    return DEFAULT_GAS_PRICE.toString()
+  } catch (e: any) {
+    throw new Error(e.message)
   }
+}
+
+export const fetchGasPrice = async (): Promise<string> => {
+  if (IS_TESTNET || CHAIN_ID === ChainId.Kovan) {
+    return new BigNumberInWei(DEFAULT_GAS_PRICE).toString()
+  }
+
+  try {
+    return await fetchGasPriceFromEthGasStation()
+  } catch (e) {
+    //
+  }
+
+  try {
+    return await fetchGasPriceFromEtherchain()
+  } catch (e) {
+    //
+  }
+
+  return new BigNumberInWei(DEFAULT_GAS_PRICE).toString()
 }
