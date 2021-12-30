ab886d6f516f8bed8283043dc623065c396c6455
thomasRalee <thomas.leera@gmail.com>
2021-12-31 05:41:08 +0800

    chore: hardcode dmm names for selected addresses

---
+++ app/data/dmm.ts
@@ -0,0 +1,6 @@
+export const hardCodedDmmNames = {
+  inj1qe8upydvr472gt4p8n35q4ym2z0xr9ttd0raqz: 'Ledger Prime 1',
+  inj1x2ck0ql2ngyxqtw8jteyc0tchwnwxv7npaungt: 'Anti Capital 1 ',
+  inj1yzmv3utcm0xx4ahsn7lyew0zzdjp4z7wlx44vx: 'Anti Capital 2 ',
+  inj1rwv4zn3jptsqs7l8lpa3uvzhs57y8duemete9e: 'Anti Capital 3 '
+} as Record<string, string>
