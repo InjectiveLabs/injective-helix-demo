c2cdcdd79913f143f7c9fdb2221ebcf50113408d
thomasRalee <thomas.leera@gmail.com>
2022-03-11 20:50:36 +0800

    fix: ust position not reflecting on trading account balance table

--- components/partials/portfolio/balances.vue
+++ components/partials/portfolio/balances.vue
@@ -51,7 +51,7 @@
             <span v-else-if="hideBalance">{{ HIDDEN_BALANCE_DISPLAY }}</span>
             <span v-else>
               {{ $t('common.available') }}
-              {{ totalSubaccountBalancesToString }} USD
+              {{ totalSubaccountAvailableBalancesToString }} USD
             </span>
           </p>
         </div>
@@ -76,7 +76,9 @@
               bankBalancesWithUsdBalance,
               subaccountBalancesWithUsdBalance,
               totalPositionsPnl,
-              totalPositionsMargin
+              totalPositionsMargin,
+              totalPositionsMarginByQuoteToken,
+              totalPositionsPnlByQuoteToken
             }"
           ></component>
         </VHocLoading>
@@ -209,7 +211,7 @@ export default Vue.extend({
       const { subaccountBalancesWithTokenAndPrice } = this
 
       return subaccountBalancesWithTokenAndPrice.map((balance) => {
-        const balanceInUsd = new BigNumberInWei(balance.availableBalance)
+        const balanceInUsd = new BigNumberInWei(balance.totalBalance)
           .toBase(balance.token.decimals)
           .times(balance.token.usdPrice)
           .toFixed(UI_DEFAULT_DISPLAY_DECIMALS)
@@ -257,6 +259,21 @@ export default Vue.extend({
       )
     },
 
+    totalSubaccountAvailableBalances(): BigNumberInBase {
+      const { subaccountBalancesWithUsdBalance } = this
+
+      return subaccountBalancesWithUsdBalance.reduce((total, balance) => {
+        const availableBalanceInUsd = new BigNumberInWei(
+          balance.availableBalance
+        )
+          .toBase(balance.token.decimals)
+          .times(balance.token.usdPrice)
+          .toFixed(UI_DEFAULT_DISPLAY_DECIMALS)
+
+        return total.plus(availableBalanceInUsd)
+      }, ZERO_IN_BASE)
+    },
+
     totalSubaccountBalancesToString(): string {
       const { totalSubaccountBalances } = this
 
@@ -273,15 +290,23 @@ export default Vue.extend({
       return totalSubaccountBalances.toFormat(UI_DEFAULT_MIN_DISPLAY_DECIMALS)
     },
 
-    totalPositionsPnl(): BigNumberInBase {
+    totalPositionsPnlByQuoteToken(): Record<string, BigNumberInBase> {
       const { markets, orderbooks, positions } = this
 
-      return positions.reduce((total, p) => {
+      const list = {} as Record<string, BigNumberInBase>
+
+      positions.forEach((p) => {
         const market = markets.find((m) => m.marketId === p.marketId)
         const orderbook = orderbooks[p.marketId]
 
         if (!market || !orderbook) {
-          return total.plus(new BigNumberInBase(ZERO_IN_BASE))
+          return
+        }
+
+        const quoteToken = market.oracleQuote.toLowerCase()
+
+        if (!list[quoteToken]) {
+          list[quoteToken] = ZERO_IN_BASE
         }
 
         const price = new BigNumberInWei(p.entryPrice).toBase(
@@ -303,36 +328,78 @@ export default Vue.extend({
               .times(executionPrice.minus(price))
               .times(p.direction === TradeDirection.Long ? 1 : -1)
 
-        return total.plus(pnl)
-      }, ZERO_IN_BASE)
+        list[quoteToken] = list[quoteToken].plus(pnl)
+      })
+
+      return list
     },
 
-    totalPositionsPnlInString(): string {
-      const { totalPositionsPnl } = this
+    totalPositionsPnl(): BigNumberInBase {
+      const {
+        totalPositionsPnlByQuoteToken,
+        subaccountBalancesWithTokenAndPrice
+      } = this
 
-      return totalPositionsPnl.toFormat(UI_DEFAULT_DISPLAY_DECIMALS)
+      return Object.entries(totalPositionsPnlByQuoteToken).reduce(
+        (total, [symbol, balance]) => {
+          const symbolFromSubaccount = subaccountBalancesWithTokenAndPrice.find(
+            (b) => b.token.symbol === symbol
+          )
+          const usdPrice = symbolFromSubaccount
+            ? symbolFromSubaccount.token.usdPrice
+            : 1
+
+          return total.plus(balance.times(usdPrice))
+        },
+        ZERO_IN_BASE
+      )
     },
 
-    totalPositionsMargin(): BigNumberInBase {
+    totalPositionsMarginByQuoteToken(): Record<string, BigNumberInBase> {
       const { markets, positions } = this
 
-      return positions.reduce((total, p) => {
+      const list = {} as Record<string, BigNumberInBase>
+
+      positions.forEach((p) => {
         const market = markets.find((m) => m.marketId === p.marketId)
 
         if (!market) {
-          return total.plus(new BigNumberInBase(ZERO_IN_BASE))
+          return
         }
 
-        return total.plus(
+        const quoteToken = market.oracleQuote.toLowerCase()
+
+        if (!list[quoteToken]) {
+          list[quoteToken] = ZERO_IN_BASE
+        }
+
+        list[quoteToken] = list[quoteToken].plus(
           new BigNumberInWei(p.margin).toBase(market.quoteToken.decimals)
         )
-      }, ZERO_IN_BASE)
+      })
+
+      return list
     },
 
-    totalPositionMarginsToString(): string {
-      const { totalPositionsMargin } = this
+    totalPositionsMargin(): BigNumberInBase {
+      const {
+        totalPositionsMarginByQuoteToken,
+        subaccountBalancesWithTokenAndPrice
+      } = this
 
-      return totalPositionsMargin.toFormat(UI_DEFAULT_DISPLAY_DECIMALS)
+      return Object.entries(totalPositionsMarginByQuoteToken).reduce(
+        (total, [symbol, balance]) => {
+          const symbolFromSubaccount = subaccountBalancesWithTokenAndPrice.find(
+            (b) => b.token.symbol === symbol
+          )
+          const usdPrice = symbolFromSubaccount
+            ? symbolFromSubaccount.token.usdPrice
+            : 1
+
+          return total.plus(balance.times(usdPrice))
+        },
+        ZERO_IN_BASE
+      )
     },
 
     tradingAccountBalances(): BigNumberInBase {
@@ -345,6 +412,12 @@ export default Vue.extend({
       return totalSubaccountBalances
         .plus(totalPositionsMargin)
         .plus(totalPositionsPnl)
+    },
+
+    totalSubaccountAvailableBalancesToString(): string {
+      const { totalSubaccountAvailableBalances } = this
+
+      return totalSubaccountAvailableBalances.toFormat(2)
     },
 
     tradingAccountBalancesToString(): string {
