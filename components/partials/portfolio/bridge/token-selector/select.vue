4904034465baaced4a4589a4a06ad3c0095d6b62
shane-moore <skm1790@gmail.com>
2022-03-17 08:36:11 -0500

    fix: portfolio ability to modify amounts in the transfer modal

--- components/partials/portfolio/bridge/token-selector/select.vue
+++ components/partials/portfolio/bridge/token-selector/select.vue
@@ -55,7 +55,7 @@
                 :value="amount"
                 @input="handleAmountChange"
                 @blur="resetIsSearching"
-                @click.native.stop="focusInput"
+                @mousedown.native.stop="focusInput"
               />
               <img
                 v-if="logo"
