fbbe2b0fdb07e1453dc2479a9686a25405315ad9
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2022-03-28 15:09:43 +0200

    chore: ape to staging

--- app/data/market.ts
+++ app/data/market.ts
@@ -1,4 +1,8 @@
-import { MarketBase, UiSpotMarketWithToken } from '@injectivelabs/ui-common'
+import {
+  MarketBase,
+  UiDerivativeMarketWithToken,
+  UiSpotMarketWithToken
+} from '@injectivelabs/ui-common'
 
 export const marketBase = {
   [MarketBase.Terra]: ['UST', 'LUNA']
@@ -36,40 +40,5 @@ export const promotedMarketSlugs = [
 export const excludedPriceDeviationSlugs = ['bayc-weth-perp'] as string[]
 
 export const upcomingMarkets = [
-  {
-    marketId: 'ape-spot-market-id',
-    marketStatus: 'active',
-    ticker: 'APE/USDT',
-    baseDenom: 'peggy0x4d224452801ACEd8B2F0aebE155379bb5D594381',
-    quoteDenom: 'peggy0xdAC17F958D2ee523a2206206994597C13D831ec7',
-    quoteToken: {
-      denom: 'peggy0xdAC17F958D2ee523a2206206994597C13D831ec7',
-      logo: '/vendor/@injectivelabs/token-metadata/usdt.svg',
-      symbol: 'USDT',
-      name: 'USDT',
-      decimals: 6,
-      address: '0xdac17f958d2ee523a2206206994597c13d831ec7',
-      coinGeckoId: 'tether'
-    },
-    baseToken: {
-      denom: 'peggy0x4d224452801ACEd8B2F0aebE155379bb5D594381',
-      logo: '/vendor/@injectivelabs/token-metadata/ape.png',
-      symbol: 'APE',
-      name: 'ApeCoin',
-      decimals: 18,
-      address: '0x4d224452801ACEd8B2F0aebE155379bb5D594381',
-      coinGeckoId: 'apecoin'
-    },
-    makerFeeRate: '0.001',
-    takerFeeRate: '0.002',
-    serviceProviderFee: '0.4',
-    minPriceTickSize: 1e-15,
-    minQuantityTickSize: 1000000000000000,
-    slug: 'ape-usdt',
-    type: 'Spot',
-    subType: 'Spot',
-    priceDecimals: 3,
-    quantityDecimals: 3,
-    upcoming: true
-  } as UiSpotMarketWithToken
-]
+  //
+] as Array<UiSpotMarketWithToken | UiDerivativeMarketWithToken>
