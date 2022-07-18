1ea50da7c5d6d1f91043ab4badb1fbad7d12d783
Bartek Tofel <bartek.tofel@ipresence.ca>
2022-07-18 17:53:04 +0200

    chore: define GH workflow for e2e tests

---
+++ .github/vue/chart.vue
@@ -0,0 +1,53 @@
+<template>
+  <div
+    :id="containerId"
+    ref="tradingView"
+    class="tv_chart_container w-full"
+  ></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import config from './config'
+
+export default Vue.extend({
+  props: {
+    symbol: {
+      required: true,
+      type: String
+    },
+
+    interval: {
+      required: true,
+      type: String
+    },
+
+    datafeedEndpoint: {
+      required: true,
+      type: String
+    }
+  },
+
+  data() {
+    return {
+      tradingView: null as any
+    }
+  },
+
+  computed: {
+    uid(): string {
+      return window.crypto.getRandomValues(new Uint32Array(1))[0].toString()
+    },
+
+    containerId(): string {
+      const { uid } = this
+
+      return `tv_chart_container-${uid}`
+    }
+  },
+
+  mounted() {
+      this.$emit('ready')
+  }
+})
+</script>
