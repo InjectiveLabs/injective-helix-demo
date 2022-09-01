54653f2072155d9b928b665d523822e142165bd6
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2022-09-01 21:05:32 +0300

    chore: copy

--- locales/en.ts
+++ locales/en.ts
@@ -49,7 +49,7 @@ export default {
     'We are constantly improving Helix. Please come back later. Follow the latest updates on our public status page.',
   token_allowance_successful: 'Token allowance set successful',
   disclaimer_note:
-    'By connecting a wallet, you agree to the Injective Labs <a href="https://injectivelabs.org/terms-and-conditions" class="text-primary-500" target="_blank" />Terms and Conditions</a>, have read the <a href="https://injectivelabs.org/privacy-policy" class="text-primary-500" target="_blank" />Privacy Policy</a> and acknowledge that you have read and understand the Injective Protocol <a href="https://injectiveprotocol.com/disclaimer" class="text-primary-500" target="_blank">disclaimer</a>.',
+    'By connecting a wallet, you agree to the Injective Labs <a href="https://injectivelabs.org/terms-and-conditions" class="text-primary-500" target="_blank" />Terms and Conditions</a>, have read the <a href="https://injectivelabs.org/privacy-policy" class="text-primary-500" target="_blank" />Privacy Policy</a> and acknowledge that you have read and understand the Helixtocol <a href="https://injectiveprotocol.com/disclaimer" class="text-primary-500" target="_blank">disclaimer</a>.',
   'There are no results found - Markets':
     'There are no markets found. You can go ahead and propose some!',
   getting_started: 'Getting Started',
@@ -80,11 +80,11 @@ export default {
   acknowledge_1:
     'You are not a person or company who is a resident of, is located, incorporated, or has a registered agent in, the United States of America or a Restricted Territory (as defined in the T&C).',
   acknowledge_2:
-    'You will not in the future access this site or use Injective.Exchange while located in the United States of America or a Restricted Territory.',
+    'You will not in the future access this site or use helixapp.com while located in the United States of America or a Restricted Territory.',
   acknowledge_3:
     'You are not using, and will not in the future use, a virtual private network or other means to mask your physical location from a Restricted Territory.',
   acknowledge_4:
-    'You are lawfully permitted to access this site and trade on Injective.Exchange under the laws of the jurisdiction in which you reside and are located.',
+    'You are lawfully permitted to access this site and trade on helixapp.com under the laws of the jurisdiction in which you reside and are located.',
   acknowledge_5:
     'You understand the risks associated with using leverage, entering into perpetual contracts, and trading in digital assets.',
   'Fee Discounts': 'Fee Discounts',
@@ -193,7 +193,7 @@ export default {
 
   marketDeprecated: {
     title: 'No longer tradable',
-    description: '{ticker} is no longer tradable on Injective Pro.',
+    description: '{ticker} is no longer tradable on Helix.',
     subDescriptionOne:
       'If you would like to continue trading {ticker}, you could still do so with other exchanges in the Injective ecosystem.',
     subDescriptionTwo:
