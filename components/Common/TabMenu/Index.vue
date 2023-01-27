650700f0e89b77478758f4a50a8754cc01084c47
thomasRalee <thomas.leera@gmail.com>
2023-01-27 14:01:38 +0800

    refactor: tab menu implementation

--- components/Common/TabMenu/Index.vue
+++ components/Common/TabMenu/Index.vue
@@ -5,7 +5,7 @@
     <div
       class="flex items-center gap-2 3md:gap-4 overflow-x-auto justify-between xs:justify-start w-full xs:w-auto hide-scrollbar mb-[-2px]"
     >
-      <slot name="items" />
+      <slot />
     </div>
     <div
       class="mt-4 sm:mt-2 md:mt-0 flex justify-start items-start sm:items-center flex-col sm:flex-row"
