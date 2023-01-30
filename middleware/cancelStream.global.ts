7d84ce56ba070bfd3d35d7ff59c21ff6f68f8abe
thomasRalee <thomas.leera@gmail.com>
2023-01-31 01:18:14 +0800

    chore: update PR base on Bojan's review

--- middleware/cancelStream.global.ts
+++ middleware/cancelStream.global.ts
@@ -1,9 +1,11 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const appStore = useAppStore()
 
-  const isSameRouteDifferentQuery =
-    to.name === from.name &&
-    JSON.stringify(to.query) !== JSON.stringify(from.query)
+  const isDifferentQuery =
+    Object.entries(to.query).sort().toString() !==
+    Object.entries(from.query).sort().toString()
+
+  const isSameRouteDifferentQuery = to.name === from.name && isDifferentQuery
 
   if (!isSameRouteDifferentQuery) {
     appStore.cancelAllStreams()
