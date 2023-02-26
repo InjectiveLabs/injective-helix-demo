351ef2338c5446dc2a06d3212288dcd5777e19d8
ParallelDEX <126363788+ParallelDEX@users.noreply.github.com>
2023-02-26 08:16:57 +0100

    Add files via upload

--- components/Asset/LogoMini.vue
+++ components/Asset/LogoMini.vue
@@ -1,30 +1,47 @@
-<template>
-  <div v-html="svgCode"></div>
-</template>
-
-<script>
-export default {
-  name: 'SvgComponent',
-  data() {
-    return {
-      svgCode: `<svg version="1.1" id="Livello_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-       viewBox="0 0 300 300" style="enable-background:new 0 0 300 300;" xml:space="preserve">
-      <style type="text/css">
-        .st0{fill:url(#SVGID_1_);}
-        .st1{fill:#FFFFFF;}
-      </style>
-      <linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="-806.008" y1="690.7227" x2="-805.008" y2="690.7227" gradientTransform="matrix(271.8923 126.7855 126.7855 -271.8923 131543.125 290058.0625)">
-        <stop  offset="0" style="stop-color:#3616AC"/>
-        <stop  offset="1" style="stop-color:#E09BED"/>
-      </linearGradient>
-      <rect class="st0" width="300" height="300"/>
-      <g id="_x35_997047a-69c2-432d-836c-d299ff0ddb69" transform="matrix(0.9816394080243864,0,0,0.9816394080243864,3.0903597561459364,103.17580697761967)">
-        <path class="st1" d="M118.3-24.6V120c0,8.6,13.4,8.6,13.4,0V-24.6C131.7-33.2,118.3-33.2,118.3-24.6z M167.6-24.6V120
-        c0,8.6,13.4,8.6,13.4,0V-24.6C181-33.2,167.6-33.2,167.6-24.6z"/>
-      </g>
-    </svg>`
-    };
-  }
-};
+<script lang="ts" setup>
+const uuid = computed(() => {
+  return Math.random()
+})
 </script>
 
+<template>
+  <svg
+    viewBox="-1 4 30 33"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    class="logo-icon"
+  >
+    <path
+      d="M3.464 11.003c.167-.215.344-.421.521-.628.008-.01.026-.012.034-.022.016-.02.042-.033.058-.053l.016-.02c.123-.117.254-.244.406-.355a6.014 6.014 0 011.677-.94c1.88-.674 3.972-.258 5.613 1.326 2.29 2.195 2.085 5.733.257 8.084-2.312 3.504-6.283 8.394-.784 12.774.989.788 1.722 1.437 4.836 2.357-2.036.384-3.925.265-6.027-.284-1.487-.859-3.825-2.696-4.62-5.178-1.203-3.763 2.116-9.389 3.72-11.555 2.202-3-1.361-6.245-3.984-2.621-1.372 1.888-3.77 7.233-2.937 11.197.487 2.248 1.137 3.887 3.713 6.138a12.182 12.182 0 01-1.391-.981c-5.986-5.7-5.291-14.514-1.108-19.239z"
+      :fill="`url(#logo_gradient1-${uuid})`"
+    ></path>
+    <path
+      d="M23.839 28.997c-.167.215-.344.421-.521.628-.008.01-.026.012-.034.022-.016.02-.042.033-.058.053l-.016.02c-.123.117-.254.244-.406.355a6.013 6.013 0 01-1.677.94c-1.88.674-3.972.258-5.613-1.326-2.29-2.195-2.085-5.733-.257-8.084 2.311-3.504 6.283-8.394.784-12.774-.989-.788-1.722-1.437-4.837-2.357 2.037-.384 3.926-.265 6.028.284 1.487.859 3.825 2.696 4.62 5.178 1.203 3.763-2.116 9.389-3.72 11.555-2.202 3 1.361 6.245 3.984 2.621 1.372-1.888 3.77-7.233 2.937-11.197-.488-2.248-1.137-3.887-3.713-6.138.477.288.941.615 1.391.981 5.985 5.7 5.291 14.514 1.108 19.239z"
+      :fill="`url(#logo_gradient2-${uuid})`"
+    ></path>
+    <defs>
+      <linearGradient
+        :id="`logo_gradient1-${uuid}`"
+        x1=".203"
+        y1="20"
+        x2="27.1"
+        y2="20"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#0082FA"></stop>
+        <stop offset="1" stop-color="#00F2FE"></stop>
+      </linearGradient>
+      <linearGradient
+        :id="`logo_gradient2-${uuid}`"
+        x1=".203"
+        y1="20"
+        x2="27.1"
+        y2="20"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#0082FA"></stop>
+        <stop offset="1" stop-color="#00F2FE"></stop>
+      </linearGradient>
+    </defs>
+  </svg>
+</template>
