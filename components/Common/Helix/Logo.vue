81ad08a0b1e3f069fa53003cff5e08a1623b678c
ParallelDEX <126363788+ParallelDEX@users.noreply.github.com>
2023-02-26 02:36:52 +0100

    Update Logo.vue

--- components/Common/Helix/Logo.vue
+++ components/Common/Helix/Logo.vue
@@ -1,18 +1,29 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!-- Generator: Adobe Illustrator 26.3.1, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
-<svg version="1.1" id="Livello_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-	 viewBox="0 0 300 300" style="enable-background:new 0 0 300 300;" xml:space="preserve">
-<style type="text/css">
-	.st0{fill:url(#SVGID_1_);}
-	.st1{fill:#FFFFFF;}
-</style>
-<linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="-806.008" y1="690.7227" x2="-805.008" y2="690.7227" gradientTransform="matrix(271.8923 126.7855 126.7855 -271.8923 131543.125 290058.0625)">
-	<stop  offset="0" style="stop-color:#3616AC"/>
-	<stop  offset="1" style="stop-color:#E09BED"/>
-</linearGradient>
-<rect class="st0" width="300" height="300"/>
-<g id="_x35_997047a-69c2-432d-836c-d299ff0ddb69" transform="matrix(0.9816394080243864,0,0,0.9816394080243864,3.0903597561459364,103.17580697761967)">
-	<path class="st1" d="M118.3-24.6V120c0,8.6,13.4,8.6,13.4,0V-24.6C131.7-33.2,118.3-33.2,118.3-24.6z M167.6-24.6V120
-		c0,8.6,13.4,8.6,13.4,0V-24.6C181-33.2,167.6-33.2,167.6-24.6z"/>
-</g>
-</svg>
+<template>
+  <div v-html="svgCode"></div>
+</template>
+
+<script>
+export default {
+  name: 'SvgComponent',
+  data() {
+    return {
+      svgCode: `<svg version="1.1" id="Livello_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+       viewBox="0 0 300 300" style="enable-background:new 0 0 300 300;" xml:space="preserve">
+      <style type="text/css">
+        .st0{fill:url(#SVGID_1_);}
+        .st1{fill:#FFFFFF;}
+      </style>
+      <linearGradient id="SVGID_1_" gradientUnits="userSpaceOnUse" x1="-806.008" y1="690.7227" x2="-805.008" y2="690.7227" gradientTransform="matrix(271.8923 126.7855 126.7855 -271.8923 131543.125 290058.0625)">
+        <stop  offset="0" style="stop-color:#3616AC"/>
+        <stop  offset="1" style="stop-color:#E09BED"/>
+      </linearGradient>
+      <rect class="st0" width="300" height="300"/>
+      <g id="_x35_997047a-69c2-432d-836c-d299ff0ddb69" transform="matrix(0.9816394080243864,0,0,0.9816394080243864,3.0903597561459364,103.17580697761967)">
+        <path class="st1" d="M118.3-24.6V120c0,8.6,13.4,8.6,13.4,0V-24.6C131.7-33.2,118.3-33.2,118.3-24.6z M167.6-24.6V120
+        c0,8.6,13.4,8.6,13.4,0V-24.6C181-33.2,167.6-33.2,167.6-24.6z"/>
+      </g>
+    </svg>`
+    };
+  }
+};
+</script>
