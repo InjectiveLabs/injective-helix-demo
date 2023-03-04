1e66c788893da8a2c6ad555760c9c141e0e1cabf
ParallelDEX <126363788+ParallelDEX@users.noreply.github.com>
2023-03-04 04:07:38 +0100

    Update FooterCta.vue

--- components/Partials/Home/FooterCta.vue
+++ components/Partials/Home/FooterCta.vue
@@ -30,7 +30,7 @@ function handleTradeClickedTrack() {
 <template>
   <div>
     <div
-      :style="{ backgroundImage: `url('/svg/wawa-area.svg')` }"
+      :style="{ backgroundImage: `url('/svg/5166956.eps')` }"
       class="bg-cover py-14 bg-left-top h-[189px] md:h-[790px] flex items-center"
     >
       <div
