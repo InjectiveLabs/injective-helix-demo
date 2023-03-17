b93a03eff4d986371842e8d141e0ae61aa1de982
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2023-03-17 04:59:14 +0100

    chore: time

--- locales/banners/en.ts
+++ locales/banners/en.ts
@@ -18,7 +18,7 @@ export default {
       tryNow: 'Get started'
     },
     'banner-sheduled-upgrade-march':
-      "A scheduled maintenance will take place at 13:00 UTC, 17 March 2023. Please note that you won't be able to place orders and manage positions during the downtime.",
+      "A scheduled maintenance will take place around 16:00 UTC, 17 March 2023. Please note that you won't be able to place orders and manage positions during the downtime.",
     userFeedback: {
       loveFeedback: "We'd love to hear your feedback!",
       thankYouBeginning:
