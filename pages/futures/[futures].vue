ce7b8b0c5295f5fd116cea350e806d1d6099188e
thomasRalee <thomas.leera@gmail.com>
2023-08-07 19:33:25 +0800

    chore: remove geoIp debug console logs

--- pages/futures/[futures].vue
+++ pages/futures/[futures].vue
@@ -14,11 +14,6 @@ definePageMeta({
       const modalStore = useModalStore()
 
       if (['US'].includes(appStore.userState.geoLocation.country)) {
-        // eslint-disable-next-line no-console
-        console.log(
-          `blocking futures market with ${appStore.userState.geoLocation.country}`
-        )
-
         modalStore.openModal({ type: Modal.FuturesMarketRestricted })
       }
     }
