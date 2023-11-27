684fa2d37742dc54c2578f49b2b3ca2724bbbc01
Ivan Angelkoski <ivanangelkoski95@gmail.com>
2023-11-28 00:44:34 +0100

    chore: campaign rules link

--- components/Partials/Liquidity/Header.vue
+++ components/Partials/Liquidity/Header.vue
@@ -95,7 +95,7 @@ const { valueToString: totalRewardsThisRoundToString } = useBigNumberFormatter(
         </NuxtLink>
 
         <NuxtLink
-          :to="'#'"
+          to="https://helixapp.zendesk.com/hc/en-us/articles/8258846181647-Share-30-000-TIA-in-TIA-Spot-Trading-Challenge-"
           target="_blank"
           class="block leading-5 py-2 px-5 font-semibold whitespace-nowrap text-white border-white hover:text-gray-300 border rounded-lg"
         >
