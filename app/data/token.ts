b78753b357fe84a40e586ffcbfdb144a5332144e
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2023-12-11 23:38:15 +0100

    chore: minor

--- app/data/token.ts
+++ app/data/token.ts
@@ -128,4 +128,4 @@ export const stableCoinDenoms = [
 export const KAVA_USDT_SYMBOL = 'USDTkv'
 export const STINJ_USDT_SYMBOL = 'STINJ'
 export const allowanceResetSymbols = ['USDT']
-export const SWAP_LOW_LIQUIDITY_SYMBOLS = ['GF', 'SOMM']
+export const SWAP_LOW_LIQUIDITY_SYMBOLS = ['GF', 'SOMM', 'NEOK']
