8d5af289681f053b5be94ceed73809024b8134fd
thomasRalee <thomas.leera@gmail.com>
2023-12-11 22:57:22 +0800

    chore: redirect campaign endpoints to staging infra

--- app/utils/constants/setup.ts
+++ app/utils/constants/setup.ts
@@ -134,9 +134,11 @@ if (endpointsNotProvided) {
 }
 
 const CAMPAIGN_ENDPOINT =
-  IS_MAINNET && !IS_STAGING
-    ? 'https://k8s.mainnet.campaigns.grpc-web.injective.network'
-    : IS_TESTNET
+  IS_MAINNET || IS_STAGING
+    ? 'https://staging.api.injective.network'
+    : // : IS_MAINNET && !IS_STAGING
+    // ? 'https://k8s.mainnet.campaigns.grpc-web.injective.network'
+    IS_TESTNET
     ? 'https://k8s.testnet.campaigns.grpc-web.injective.network'
     : endpoints.indexer
 
