fa96bcdf6a80c570e3c6443d4a72351d809f73c2
thomasRalee <thomas.leera@gmail.com>
2024-01-17 05:18:56 +0800

    chore: adapt token metadata refactor

--- app/data/token.ts
+++ app/data/token.ts
@@ -1,15 +1,12 @@
-import {
-  getChecksumAddress,
-  CW20_ADAPTER_CONTRACT_BY_NETWORK
-} from '@injectivelabs/sdk-ts'
 import { INJ_DENOM } from '@injectivelabs/utils'
 import { BridgingNetwork } from '@injectivelabs/sdk-ui-ts'
 import type { Token, TokenWithPrice } from '@injectivelabs/token-metadata'
-import { NETWORK } from '@/app/utils/constants'
-import { denomClient, tokenMetaUtils } from '@/app/Services'
+import { tokenMetaUtils } from '@/app/Services'
+import {
+  getCw20FromSymbolOrNameAsString,
+  getPeggyDenomFromSymbolOrNameAsString
+} from '@/app/utils/helper'
 import { USDCSymbol } from '@/types'
-
-const adapterContract = CW20_ADAPTER_CONTRACT_BY_NETWORK[NETWORK]
 
 export const injToken = {
   denom: INJ_DENOM,
@@ -50,68 +47,28 @@ export const tokenToDecimalsOverrideMap = {
   [TokenSymbols.WETH]: 5
 }
 
-export const getFactoryDenomFromDenom = (address: string): string =>
-  `factory/${adapterContract}/${address}`
-
 export const getDenomsFromToken = (token: Token): string[] => {
-  const cw20sDenom = (token.cw20s || []).map(({ address }) =>
-    getFactoryDenomFromDenom(address)
-  )
   const cw20Denom = token.cw20
-    ? getFactoryDenomFromDenom(token.cw20.address)
+    ? getCw20FromSymbolOrNameAsString(token.cw20.address)
     : ''
   const ibc20Denom = token.ibc ? `ibc/${token.ibc.hash}` : ''
   const peggyDenom = token.erc20 ? `peggy${token.erc20.address}` : ''
 
-  const denoms = [
-    cw20Denom,
-    ibc20Denom,
-    peggyDenom,
-    token.denom,
-    ...cw20sDenom
-  ].filter((denom) => denom)
+  const denoms = [cw20Denom, ibc20Denom, peggyDenom, token.denom].filter(
+    (denom) => denom
+  )
 
   return [...new Set(denoms)]
 }
 
-export const getFactoryDenomFromSymbol = (symbol: USDCSymbol) => {
-  const tokenMeta = denomClient.getTokenMetaDataBySymbol(symbol)
-
-  if (!tokenMeta || !adapterContract) {
-    return ''
-  }
-
-  if (tokenMeta.cw20) {
-    return getFactoryDenomFromDenom(tokenMeta.cw20.address)
-  }
-
-  const cw20TokenMeta = (tokenMeta.cw20s || []).find(
-    (cw20) => cw20.symbol.toLowerCase() === symbol.toLowerCase()
-  )
-
-  if (!cw20TokenMeta) {
-    return ''
-  }
-
-  return getFactoryDenomFromDenom(cw20TokenMeta.address)
-}
-
-export const getPeggyDenomFromSymbol = (symbol: USDCSymbol) => {
-  const tokenMeta = denomClient.getTokenMetaDataBySymbol(symbol)
-
-  if (!tokenMeta || !tokenMeta.erc20) {
-    return ''
-  }
-
-  return `peggy${getChecksumAddress(tokenMeta.erc20.address)}`
-}
-
 export const usdcTokenDenom = {
-  [USDCSymbol.PeggyEthereum]: getPeggyDenomFromSymbol(USDCSymbol.PeggyEthereum),
-  [USDCSymbol.WormholeEthereum]: getFactoryDenomFromSymbol(
+  [USDCSymbol.PeggyEthereum]: getPeggyDenomFromSymbolOrNameAsString(
+    USDCSymbol.PeggyEthereum
+  ),
+  [USDCSymbol.WormholeEthereum]: getCw20FromSymbolOrNameAsString(
     USDCSymbol.WormholeEthereum
   ),
-  [USDCSymbol.WormholeSolana]: getFactoryDenomFromSymbol(
+  [USDCSymbol.WormholeSolana]: getCw20FromSymbolOrNameAsString(
     USDCSymbol.WormholeSolana
   )
 }
---
+++ app/utils/helper.ts
@@ -0,0 +1,18 @@
+import {
+  getCw20FromSymbolOrName,
+  getIbcDenomFromSymbolOrName,
+  getPeggyDenomFromSymbolOrName
+} from '@injectivelabs/token-metadata'
+import { NETWORK } from '@/app/utils/constants'
+
+/** used for tokens that we know exist in the token-metadata package so we don't do type-casting */
+export const getIbcDenomFromSymbolOrNameAsString = (symbolOrName: string) =>
+  getIbcDenomFromSymbolOrName(symbolOrName, NETWORK) as string
+
+/** used for tokens that we know exist in the token-metadata package so we don't do type-casting */
+export const getCw20FromSymbolOrNameAsString = (symbolOrName: string) =>
+  getCw20FromSymbolOrName(symbolOrName, NETWORK) as string
+
+/** used for tokens that we know exist in the token-metadata package so we don't do type-casting */
+export const getPeggyDenomFromSymbolOrNameAsString = (symbolOrName: string) =>
+  getPeggyDenomFromSymbolOrName(symbolOrName, NETWORK) as string
