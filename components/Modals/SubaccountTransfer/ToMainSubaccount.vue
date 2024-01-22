42a3c977d6cfb229009178f18cd1078604c502fa
Ivan Angelkoski <ivanangelkoski95@gmail.com>
2024-01-22 16:52:50 +0100

    chore: minor

--- components/Modals/SubaccountTransfer/ToMainSubaccount.vue
+++ components/Modals/SubaccountTransfer/ToMainSubaccount.vue
@@ -50,32 +50,34 @@ function onCloseModal() {
     :is-open="modalStore.modals[Modal.TransferToMainSubaccount]"
     @modal:closed="onCloseModal"
   >
+    <template #title>
+      <h3>
+        {{ $t('account.subaccountTransfer') }}
+      </h3>
+    </template>
+
     <div>
       <div>
-        <div class="flex justify-center pb-4">
-          <BaseIcon name="info" class="w-10 h-10 text-gray-500" />
-        </div>
-
         <h3 class="mb-8 font-semibold">
           {{ $t('sgt.accountEndBot') }}
         </h3>
 
         <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
           <AppButton
-            is-lg
-            class="w-full shadow-none select-none text-red-500 border-red-500"
-            @click="onCloseModal"
-          >
-            {{ $t('sgt.cancel') }}
-          </AppButton>
-
-          <AppButton
             v-bind="{ status }"
             is-lg
-            class="w-full shadow-none select-none text-white-500 border-red-500 bg-red-500"
+            class="w-full shadow-none select-none text-red-900 border-red-500 bg-red-500"
             @click="onEndBot"
           >
             {{ $t('sgt.endBot') }}
+          </AppButton>
+
+          <AppButton
+            is-lg
+            class="w-full shadow-none select-none text-blue-500 border-blue-500"
+            @click="onCloseModal"
+          >
+            {{ $t('sgt.cancel') }}
           </AppButton>
         </div>
       </div>
