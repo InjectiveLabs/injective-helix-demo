343b3a0fd862cb125d1d15049beff360e922baed
Ivan Angelkoski <ivanangelkoski95@gmail.com>
2024-02-07 19:53:03 +0100

    chore: disable claims

--- components/Partials/Liquidity/Dashboard/Round/Table/Row.vue
+++ components/Partials/Liquidity/Dashboard/Round/Table/Row.vue
@@ -88,6 +88,8 @@ const { valueToString: marketVolumeInUsdToString } = useBigNumberFormatter(
     decimalPlaces: UI_DEFAULT_MIN_DISPLAY_DECIMALS
   }
 )
+
+const isRound10 = computed(() => props.campaign.roundId === 10)
 </script>
 
 <template>
@@ -135,7 +137,11 @@ const { valueToString: marketVolumeInUsdToString } = useBigNumberFormatter(
           v-bind="{
             campaign
           }"
+          :force-disabled="isRound10"
         />
+        <p v-if="isRound10" class="text-xs text-gray-500">
+          (Available To Claim Soon)
+        </p>
       </div>
     </td>
   </tr>
