4f2817477690953185d27a37ba77e3120a510c49
Ivan Angelkoski <ivanangelkoski95@gmail.com>
2024-02-07 19:06:21 +0100

    chore: subaccount composable init

---
+++ composables/account/useSubaccounts.ts
@@ -0,0 +1,44 @@
+import {
+  getMarketSlugFromSubaccountId,
+  getSubaccountIndex,
+  isSgtSubaccountId
+} from '@/app/utils/helpers'
+
+export function useSubaccounts() {
+  const route = useRoute()
+  const accountStore = useAccountStore()
+  const { t } = useLang()
+
+  const isSpotOrFuturesRoute = computed(() =>
+    ['spot', 'futures'].some((r) => (route.name as string).startsWith(r))
+  )
+
+  const subaccountSelectOptions = computed(() =>
+    accountStore.hasMultipleSubaccounts
+      ? Object.keys(accountStore.subaccountBalancesMap)
+          .filter((subaccountId) =>
+            isSpotOrFuturesRoute.value ? !isSgtSubaccountId(subaccountId) : true
+          )
+          .map((value) => {
+            if (getSubaccountIndex(value) === 0) {
+              return { display: `${t('account.main')}`, value }
+            }
+
+            if (isSgtSubaccountId(value)) {
+              return {
+                value,
+                display: `SGT ${getMarketSlugFromSubaccountId(value)}`
+              }
+            }
+
+            return {
+              value,
+              display: getSubaccountIndex(value).toString()
+            }
+          })
+          .sort((a, b) => a.value.localeCompare(b.value))
+      : []
+  )
+
+  return { subaccountSelectOptions }
+}
