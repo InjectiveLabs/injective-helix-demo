8edd65cb8944e18440ba474a73d666713a7bd3f7
Ivan Angelkoski <ivanangelkoski95@gmail.com>
2024-02-25 19:16:39 +0100

    chore: ethdenver

---
+++ locales/ethdenver/en.ts
@@ -0,0 +1,14 @@
+export default {
+  ethdenver: {
+    banner: 'Injective x ETHDenver 2024',
+    title: 'Special campaign for new Helix users at ETHDenver',
+    subtitle: '10 USDT rebate for the first 100 users!',
+    step1: 'Join the campaign',
+    step2: 'Place more than 100 USDT worth of trades',
+    step3: 'Results to be announced on March 5 2024',
+    register: 'Register',
+    goToSwap: 'Go to Swap',
+    footer:
+      '*Only applicable to users who have not used Helix prior Feb 23 2024'
+  }
+}
---
+++ pages/ethdenver2024.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+  <div class="pt-20">
+    <div class="max-w-xl mx-auto">
+      <div
+        class="bg-[url('/svg/ethdenver2024.svg')] rounded-xl bg-cover mx-auto text-center py-10 px-4"
+      >
+        <h1 class="text-xl font-bold">{{ $t('ethdenver.banner') }}</h1>
+      </div>
+
+      <div class="text-center my-4 space-y-4">
+        <h1 class="text-xl font-semibold">{{ $t('ethdenver.title') }}</h1>
+        <h1 class="text-lg">{{ $t('ethdenver.subtitle') }}</h1>
+      </div>
+
+      <div class="bg-gray-800 rounded-xl p-8 space-y-4">
+        <div class="flex justify-center items-center font-semibold space-x-2">
+          <div
+            class="bg-blue-500 text-white w-10 h-10 rounded-full flex items-center justify-center"
+          >
+            1
+          </div>
+          <p>{{ $t('ethdenver.step1') }}</p>
+        </div>
+
+        <div class="flex justify-center">
+          <AppButton class="bg-blue-500 text-white border-blue-500">
+            {{ $t('ethdenver.register') }}
+          </AppButton>
+        </div>
+
+        <div class="flex justify-center items-center font-semibold space-x-2">
+          <div
+            class="bg-blue-500 text-white w-10 h-10 rounded-full flex items-center justify-center"
+          >
+            2
+          </div>
+          <p>{{ $t('ethdenver.step2') }}</p>
+        </div>
+
+        <div>
+          <div class="flex justify-center">
+            <NuxtLink to="/swap">
+              <AppButton class="bg-blue-500 text-white border-blue-500">
+                {{ $t('ethdenver.goToSwap') }}
+              </AppButton>
+            </NuxtLink>
+          </div>
+        </div>
+
+        <div class="flex justify-center items-center font-semibold space-x-2">
+          <div
+            class="bg-blue-500 text-white w-10 h-10 rounded-full flex items-center justify-center"
+          >
+            3
+          </div>
+          <p>{{ $t('ethdenver.step3') }}</p>
+        </div>
+      </div>
+
+      <p class="text-gray-500 text-center mt-8">{{ $t('ethdenver.footer') }}</p>
+    </div>
+  </div>
+</template>
