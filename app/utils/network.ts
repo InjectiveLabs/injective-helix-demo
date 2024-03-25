130b9c7f70f1363550a89433a9cfe1c06b3a6549
shane-moore <skm1790@gmail.com>
2024-03-25 11:50:43 -0700

    chore: add new bridge prod url

--- app/utils/network.ts
+++ app/utils/network.ts
@@ -12,15 +12,10 @@ export const getExplorerUrl = (): string => {
   return 'https://explorer.injective.network'
 }
 
-// todo: update testnet and mainnet url when bridge goes live
 export const getBridgeUrl = (): string => {
   if (IS_DEVNET) {
     return 'https://devnet.bridge.injective.dev'
   }
 
-  if (IS_TESTNET) {
-    return 'https://devnet.bridge.injective.dev'
-  }
-
-  return 'https://devnet.bridge.injective.dev'
+  return 'https://bridge.injective.network'
 }
