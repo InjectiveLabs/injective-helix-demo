0aeaef739d10be5c41b7bd8eaf0bce9c79a0a7f0
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2024-05-28 02:07:58 +0200

    fix: bulletin

--- pages/liquidity-provision.vue
+++ pages/liquidity-provision.vue
@@ -68,10 +68,14 @@ const vaults = computed(() =>
 
 <template>
   <div class="container py-10">
-    <h2 class="text-2xl font-semibold">{{ $t('liquidityProvision.title') }}</h2>
-    <p class="text-gray-300 mt-2">{{ $t('liquidityProvision.description') }}</p>
-
     <AppHocLoading v-bind="{ status }">
+      <h2 class="text-2xl font-semibold">
+        {{ $t('liquidityProvision.title') }}
+      </h2>
+      <p class="text-gray-300 mt-2">
+        {{ $t('liquidityProvision.description') }}
+      </p>
+
       <div
         class="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-4"
       >
