db2665d4fafa6d789a05265b9b2a6c6ac6ef2c5e
thomasRalee <thomas.leera@gmail.com>
2024-09-02 16:46:16 +0800

    chore: minor

--- components/Modals/QRCode.vue
+++ components/Modals/QRCode.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { sharedEllipsisFormatText } from '@shared/utils/formatter'
 import { Modal } from '@/types'
 
 const modalStore = useModalStore()
@@ -8,6 +9,10 @@ const { t } = useLang()
 const { copy } = useClipboard()
 
 const isModalOpen = computed(() => modalStore.modals[Modal.QrCode])
+
+const formattedAddress = computed(() =>
+  sharedEllipsisFormatText(sharedWalletStore.injectiveAddress, 12)
+)
 
 function closeModal() {
   modalStore.closeModal(Modal.QrCode)
@@ -27,16 +32,16 @@ function onCopyAddress() {
     @modal:closed="closeModal"
   >
     <section class="text-center">
-      <h3 class="text-lg font-semibold">{{ $t('common.qrCode') }}</h3>
+      <!-- <h3 class="text-lg font-semibold">{{ $t('common.qrCode') }}</h3> -->
 
       <SharedQRCodeNew
-        class="max-w-[280px] w-full mx-auto rounded-lg overflow-hidden mt-10"
+        class="max-w-[280px] w-full mx-auto rounded-lg overflow-hidden mt-4"
         :text="sharedWalletStore.injectiveAddress"
       />
 
       <div class="flex items-center gap-2 max-w-[384px] mt-6">
         <p class="text-ellipsis overflow-hidden">
-          {{ sharedWalletStore.injectiveAddress }}
+          {{ formattedAddress }}
         </p>
         <AppButton @click="onCopyAddress">{{ $t('common.copy') }}</AppButton>
       </div>
