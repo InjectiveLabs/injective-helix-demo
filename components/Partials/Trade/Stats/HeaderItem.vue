b36d086874e1551dd071e0ee67511bc648cccde6
thomasRalee <thomas.leera@gmail.com>
2024-09-05 00:55:52 +0800

    chore: annualized funding rate pg-138

---
+++ components/Partials/Trade/Stats/HeaderItem.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+withDefaults(
+  defineProps<{
+    title?: string
+  }>(),
+  {
+    title: ''
+  }
+)
+</script>
+
+<template>
+  <div
+    class="flex max-lg:justify-between text-xs lg:flex-col lg:justify-center lg:gap-y-2 lg:p-2"
+  >
+    <slot name="title">
+      <span v-if="title" class="text-gray-400">{{ title }}</span>
+    </slot>
+
+    <slot />
+  </div>
+</template>
