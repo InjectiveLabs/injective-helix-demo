7ed79004e4d00926456d0f0c66f4bc8ec78b520d
Ivan Angelkoski <ivanangelkoski95@gmail.com>
2024-09-06 16:15:37 +0200

    refactor: btn variant tw

--- components/App/Button.vue
+++ components/App/Button.vue
@@ -5,30 +5,42 @@ import { twMerge } from 'tailwind-merge'
 import { Status, StatusType } from '@injectivelabs/utils'
 
 const button = cva(
-  'flex items-center justify-center transition-all rounded-md',
+  'flex items-center justify-center transition-all rounded-md font-semibold font-sans border disabled:cursor-not-allowed',
   {
     variants: {
       size: {
-        xs: 'btn-xs',
-        sm: 'btn-sm',
-        md: 'btn-md',
-        lg: 'btn-lg'
+        xs: 'py-1 px-2 text-xs',
+        sm: 'py-1 px-3 text-xs',
+        md: 'py-2 px-6 text-sm',
+        lg: 'py-2 px-8 text-sm'
       },
       variant: {
-        primary: 'btn-primary focus-within:ring-[3px] ring-blue-700',
+        primary:
+          'bg-blue-500 text-blue-900 border-blue-500 hover:bg-blue-500/70 hover:border-blue-500/70 disabled:bg-transparent disabled:text-gray-400 disabled:border-blue-500 focus-within:ring-[3px] ring-blue-700',
+
         'primary-outline':
-          'btn-primary-outline focus-within:ring-[3px] ring-blue-700',
+          'bg-transparent text-gray-200 border-blue-500 hover:text-white hover:bg-blue-500/20 disabled:bg-transparent disabled:text-gray-400 disabled:border-blue-500 focus-within:ring-[3px] ring-blue-700',
+
         'primary-ghost':
-          'btn-primary-ghost focus-within:ring-[3px] ring-blue-700',
-        danger: 'btn-danger focus-within:ring-[3px] ring-red-700',
+          'bg-transparent text-gray-200 border-transparent hover:text-white hover:bg-blue-500/20 disabled:bg-transparent disabled:text-gray-400 disabled:border-blue-500 focus-within:ring-[3px] ring-blue-700',
+
+        danger:
+          'bg-red-500 text-red-900 border-red-500 hover:text-red-900 hover:bg-red-500/70 disabled:bg-transparent disabled:text-gray-400 disabled:border-red-500 focus-within:ring-[3px] ring-red-700',
+
         'danger-outline':
-          'btn-danger-outline focus-within:ring-[3px] ring-red-700',
-        'danger-ghost': 'btn-danger-ghost focus-within:ring-[3px] ring-red-700',
-        success: 'btn-success focus-within:ring-[3px] ring-green-700',
+          'bg-transparent text-gray-200 border-red-500 hover:text-white hover:bg-red-500/20 disabled:bg-transparent disabled:text-gray-400 disabled:border-red-500 focus-within:ring-[3px] ring-red-700',
+
+        'danger-ghost':
+          'bg-transparent text-gray-200 border-transparent hover:text-white hover:bg-red-500/20 disabled:bg-transparent disabled:text-gray-400 disabled:border-red-500 focus-within:ring-[3px] ring-red-700',
+
+        success:
+          'bg-green-500 text-green-900 border-green-500 hover:text-green-900 hover:bg-green-500/70 disabled:bg-transparent disabled:text-gray-400 disabled:border-green-500 focus-within:ring-[3px] ring-green-700',
+
         'success-outline':
-          'btn-success-outline focus-within:ring-[3px] ring-green-700',
+          'bg-transparent text-gray-200 border-green-500 hover:text-white hover:bg-green-500/20 disabled:bg-transparent disabled:text-gray-400 disabled:border-green-500 focus-within:ring-[3px] ring-green-700',
+
         'success-ghost':
-          'btn-success-ghost focus-within:ring-[3px] ring-green-700'
+          'bg-transparent text-gray-200 border-transparent hover:text-white hover:bg-green-500/20 disabled:bg-transparent disabled:text-gray-400 disabled:border-green-500 focus-within:ring-[3px] ring-green-700'
       }
     }
   }
