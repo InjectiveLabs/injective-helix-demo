cf191765954499dc073fbf7ebd8e110f095e7d7d
Billy Jacoby <billyjacoby@gmail.com>
2024-09-23 14:05:55 -0400

    fix: move server address into constants

--- app/client/connectMobile.ts
+++ app/client/connectMobile.ts
@@ -1,7 +1,5 @@
 import { HttpClient } from '@injectivelabs/utils'
-
-export const CONNECT_SERVER_URL = 'http://10.0.1.60:3005'
-// const CONNECT_SERVER_URL = 'https://api.express.injective.dev/'
+import { CONNECT_SERVER_URL } from '../utils/constants'
 
 export const client = new HttpClient(CONNECT_SERVER_URL)
 
--- app/utils/constants/index.ts
+++ app/utils/constants/index.ts
@@ -165,3 +165,5 @@ export const LEGACY_MARKET_TO_CURRENT_MARKET_ID_MAP = Object.entries(
   },
   {} as Record<string, string>
 )
+
+export const CONNECT_SERVER_URL = 'https://api.express.injective.dev/'
