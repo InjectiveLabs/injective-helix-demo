e4fe7af5d2b1f1412be2dc952929d20feb6d7041
shane-moore <skm1790@gmail.com>
2024-09-30 09:58:25 -0700

    chore: update version copy

--- components/Layout/StatusBar.vue
+++ components/Layout/StatusBar.vue
@@ -14,11 +14,13 @@ const markets = computed(() =>
     <div class="flex items-center bg-brand-900 border-t px-4 py-1 text-xs z-30">
       <div class="w-2 h-2 rounded-full bg-green-500 mr-2" />
       <div class="divide-x-2 border-white flex">
-        <div class="px-2">Operational</div>
+        <div class="px-2">
+          {{ $t('banners.footer.operational') }}
+        </div>
         <div
           class="px-2 bg-gradient-to-r from-blue-500 to-blue-200 bg-clip-text text-transparent"
         >
-          Version 2.0 Alpha (New)
+          {{ $t('banners.footer.v2') }}
         </div>
       </div>
 
