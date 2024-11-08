d80cfe0fee4ef75abb72343e3e142f849b303ca4
thomasRalee <thomas.leera@gmail.com>
2024-11-08 17:08:14 +0700

    chore: update layer branch to master

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
   extends: [
     isLocalLayer
       ? '../injective-ui/layer'
-      : 'github:InjectiveLabs/injective-ui/layer#feat/nuxt-ui'
+      : 'github:InjectiveLabs/injective-ui/layer#master'
   ],
 
   app: {
