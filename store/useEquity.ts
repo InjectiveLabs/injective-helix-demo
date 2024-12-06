83d942def33b2d64ec6f9ba37a76148e761a7b49
thomasRalee <thomas.leera@gmail.com>
2024-12-07 00:34:06 +0800

    chore: uadd positionTotalPNL calculations in useEquity composable

--- store/useEquity.ts
+++ store/useEquity.ts
@@ -1,14 +1,16 @@
+import { usdtToken } from '@shared/data/token'
 import { ZERO_IN_BASE } from '@shared/utils/constant'
 import { BigNumberInBase } from '@injectivelabs/utils'
 import { SharedBalanceWithToken } from '@shared/types'
+import { TradeDirection } from '@injectivelabs/ts-types'
 import { getCw20AddressFromDenom } from '@/app/utils/helpers'
 import { verifiedDenoms } from '@/app/json'
 
 export function useEquity() {
   const tokenStore = useTokenStore()
   const accountStore = useAccountStore()
-  // const positionStore = usePositionStore()
-  // const derivativeStore = useDerivativeStore()
+  const positionStore = usePositionStore()
+  const derivativeStore = useDerivativeStore()
 
   const spotBalances = computed(() => {
     const activeSubaccountBalances =
@@ -43,7 +45,41 @@ export function useEquity() {
     }, [] as SharedBalanceWithToken[])
   })
 
-  // activeSubaccountBalances => spotBalances
+  const positionTotalMargin = computed(() => {})
+
+  const positionTotalPNL = computed(() => {
+    return positionStore.positions.reduce((sum, position) => {
+      const market = derivativeStore.markets.find(
+        (m) => m.marketId === position.marketId
+      )
+
+      if (!market) {
+        return sum
+      }
+
+      const markPrice = derivativeStore.marketMarkPriceMap[market.marketId]
+        .price
+        ? new BigNumberInBase(
+            derivativeStore.marketMarkPriceMap[market.marketId].price
+          )
+        : sharedToBalanceInTokenInBase({
+            value: position.markPrice,
+            decimalPlaces: market.quoteToken.decimals
+          })
+
+      const price = sharedToBalanceInTokenInBase({
+        value: position.entryPrice,
+        decimalPlaces: market.quoteToken.decimals
+      })
+
+      const pnl = new BigNumberInBase(position.quantity)
+        .times(markPrice.minus(price))
+        .times(position.direction === TradeDirection.Long ? 1 : -1)
+
+      return sum.plus(pnl)
+    }, ZERO_IN_BASE)
+  })
+
   const spotTotalUsdBalance = computed(() =>
     spotBalances.value.reduce((total, balance) => {
       const tokenUsdPrice = tokenStore.tokenUsdPrice(balance.token)
@@ -54,13 +90,15 @@ export function useEquity() {
     }, ZERO_IN_BASE)
   )
 
-  const positionTotalMargin = computed(() => {})
-  const positionTotalPNL = computed(() => {})
+  const positionTotalPNLUsd = computed(() =>
+    positionTotalPNL.value.times(tokenStore.tokenUsdPrice(usdtToken))
+  )
 
   return {
     spotBalances,
     positionTotalPNL,
     spotTotalUsdBalance,
-    positionTotalMargin
+    positionTotalMargin,
+    positionTotalPNLUsd
   }
 }
