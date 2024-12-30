3cd1b9741aef279a924025d842780ebdd4c22658
ivan-angjelkoski <ivanangelkoski95@gmail.com>
2024-12-30 13:27:42 +0100

    feat: active/removed perp strategies section

--- components/Partials/Trade/Futures/Orders/TradingBots/Header.vue
+++ components/Partials/Trade/Futures/Orders/TradingBots/Header.vue
@@ -1,20 +1,94 @@
 <script setup lang="ts">
-import { PerpOrdersTradingBotsView } from '@/types'
+import { SharedDropdownOption } from '@shared/types'
+import { PerpOrdersTradingBotsView, PerpetualMarketCyTags } from '@/types'
 
-const view = ref(PerpOrdersTradingBotsView.ActiveStrategies)
+const breakpoints = useBreakpointsTw()
+const gridStrategyStore = useGridStrategyStore()
+
+const lg = breakpoints['3xl']
+
+const props = withDefaults(
+  defineProps<{
+    modelValue: PerpOrdersTradingBotsView
+  }>(),
+  {}
+)
+
+const emit = defineEmits<{
+  'update:modelValue': [value: PerpOrdersTradingBotsView]
+}>()
+
+const view = useVModel(props, 'modelValue', emit)
+
+const options = computed(() => {
+  const items: SharedDropdownOption[] = [
+    {
+      display: `activity.${PerpOrdersTradingBotsView.ActiveStrategies}`,
+      value: PerpOrdersTradingBotsView.ActiveStrategies,
+      description: `${gridStrategyStore.activeDerivativeStrategies.length}`
+    },
+    {
+      display: `activity.${PerpOrdersTradingBotsView.RemovedStrategies}`,
+      value: PerpOrdersTradingBotsView.RemovedStrategies,
+      description: `${gridStrategyStore.removedStrategies.length}`
+    }
+  ]
+
+  return items
+})
 </script>
 
 <template>
   <div class="h-header border-b-2 flex sticky top-0 bg-coolGray-975 z-10">
-    <AppButtonSelect
-      v-for="value in Object.values(PerpOrdersTradingBotsView)"
-      :key="value"
+    <AppTabSelect
+      v-if="!lg"
+      v-bind="{
+        options
+      }"
       v-model="view"
-      v-bind="{ value }"
-      class="flex items-center px-4 tab-field"
-      active-classes="!text-white"
+      class="border-r"
     >
-      {{ $t(`activity.${value}`) }}
-    </AppButtonSelect>
+      <template #default="{ selected }">
+        <button
+          class="px-2"
+          :data-cy="dataCyTag(PerpetualMarketCyTags.OrderDetailsDropdown)"
+        >
+          {{ $t(selected?.display || '') }}
+          {{
+            Number.isInteger(Number(selected?.description))
+              ? `(${selected?.description || 0})`
+              : ''
+          }}
+        </button>
+      </template>
+
+      <template #option="{ option }">
+        <button
+          :data-cy="`${dataCyTag(
+            PerpetualMarketCyTags.OrderDetailsDropdownOptions
+          )}-${option.display}`"
+        >
+          {{ $t(option.display) }}
+          {{
+            Number.isInteger(Number(option.description))
+              ? `(${option.description})`
+              : ''
+          }}
+        </button>
+      </template>
+    </AppTabSelect>
+
+    <template v-else>
+      <AppButtonSelect
+        v-for="value in Object.values(PerpOrdersTradingBotsView)"
+        :key="value"
+        v-model="view"
+        v-bind="{ value }"
+        class="flex items-center px-4 tab-field"
+        active-classes="!text-white"
+      >
+        {{ $t(`activity.${value}`) }}
+      </AppButtonSelect>
+    </template>
   </div>
 </template>
--- components/Partials/Trade/Futures/Orders/TradingBots/Index.vue
+++ components/Partials/Trade/Futures/Orders/TradingBots/Index.vue
@@ -1,11 +1,37 @@
 <script setup lang="ts">
+import { Status, StatusType } from '@injectivelabs/utils'
+import { PerpOrdersTradingBotsView } from '@/types'
+
 const gridStrategyStore = useGridStrategyStore()
 
-onWalletConnected(gridStrategyStore.fetchAllStrategies)
+const view = ref(PerpOrdersTradingBotsView.ActiveStrategies)
+const status = reactive(new Status(StatusType.Loading))
+const { $onError } = useNuxtApp()
+
+onWalletConnected(fetchStrategies)
+
+function fetchStrategies() {
+  status.setLoading()
+
+  gridStrategyStore
+    .fetchAllStrategies()
+    .catch($onError)
+    .finally(() => {
+      status.setIdle()
+    })
+}
 </script>
 
 <template>
   <div>
-    <PartialsTradeFuturesOrdersTradingBotsHeader />
+    <PartialsTradeFuturesOrdersTradingBotsHeader v-model="view" />
+
+    <PartialsTradeFuturesOrdersTradingBotsRunning
+      v-if="view === PerpOrdersTradingBotsView.ActiveStrategies"
+    />
+
+    <PartialsTradeFuturesOrdersTradingBotsHistory
+      v-else-if="view === PerpOrdersTradingBotsView.RemovedStrategies"
+    />
   </div>
 </template>
