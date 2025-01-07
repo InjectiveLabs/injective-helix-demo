6240162b48f44122cbe3fa8e392c4bccbefc70ec
ivan-angjelkoski <ivanangelkoski95@gmail.com>
2025-01-07 19:01:49 +0100

    chore: change layer branch to master

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
   extends: [
     isLocalLayer
       ? '../injective-ui/layer'
-      : 'github:InjectiveLabs/injective-ui/layer#chore/auto-sign-fallback'
+      : 'github:InjectiveLabs/injective-ui/layer#master'
   ],
 
   app: {
