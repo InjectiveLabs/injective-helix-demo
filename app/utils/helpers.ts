f68de7207058b6e43091f3fccd4ea5d014cb4e88
ivan-angjelkoski <ivanangelkoski95@gmail.com>
2025-01-08 11:21:28 +0100

    chore: change chronos endpoints to k8s

--- app/utils/helpers.ts
+++ app/utils/helpers.ts
@@ -49,7 +49,7 @@ export const getDecimalsBasedOnNumber = (
 export const getChronosDatafeedEndpoint = (marketType: string): string => {
   // Todo: Replace with actual endpoint once devops deploy this to production server
   if (IS_MAINNET) {
-    return `https://k8s.global.mainnet.chronos.grpc-web.injective.network/api/chronos/v1/${marketType}`
+    return `https://k8s.mainnet.exchange.grpc-web.injective.network/api/chronos/v1/${marketType}`
   }
 
   return `${ENDPOINTS.indexer}/api/chronos/v1/${marketType}`
