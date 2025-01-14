8da56f9582f30e13f6d1054c7ddd2b69d87b4a9c
ivan-angjelkoski <ivanangelkoski95@gmail.com>
2025-01-14 15:41:15 +0100

    chore: enable light charts

--- components/Partials/Trade/Chart/Index.vue
+++ components/Partials/Trade/Chart/Index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-// import { intervalOptions } from '@/app/utils/constants'
+import { intervalOptions } from '@/app/utils/constants'
 import { BusEvents, ChartViewOption, UiMarketWithToken } from '@/types'
 
 withDefaults(
@@ -12,7 +12,7 @@ withDefaults(
   }
 )
 
-// const interval = ref(4)
+const interval = ref(4)
 const view = ref(ChartViewOption.Chart)
 
 const viewOptions = Object.values(ChartViewOption)
@@ -21,9 +21,9 @@ function onUpdateChart(chart: string) {
   useEventBus(BusEvents.UpdateMarketChart).emit(chart)
 }
 
-// function setInterval(index: string) {
-//   interval.value = Number(index)
-// }
+function setInterval(index: string) {
+  interval.value = Number(index)
+}
 </script>
 
 <template>
@@ -47,7 +47,7 @@ function onUpdateChart(chart: string) {
     </div>
 
     <!-- Light Trading Chart -->
-    <!-- <div v-if="view === ChartViewOption.Chart" class="border-b flex">
+    <div v-if="view === ChartViewOption.Chart" class="border-b flex">
       <AppButtonSelect
         v-for="(_, index) in intervalOptions"
         :key="index"
@@ -61,9 +61,9 @@ function onUpdateChart(chart: string) {
       >
         {{ intervalOptions[Number(index)].label }}
       </AppButtonSelect>
-    </div> -->
+    </div>
 
-    <!-- <PartialsTradingLightTradingChartWrapper
+    <PartialsTradingLightTradingChartWrapper
       v-if="view === ChartViewOption.Chart"
       v-bind="{
         market: market as UiMarketWithToken,
@@ -71,12 +71,12 @@ function onUpdateChart(chart: string) {
         isSpot,
         interval
       }"
-    /> -->
+    />
 
-    <PartialsTradingMarketChart
+    <!-- <PartialsTradingMarketChart
       v-if="view === ChartViewOption.Chart"
       v-bind="{ market }"
-    />
+    /> -->
 
     <PartialsTradeChartDepth v-else-if="view === ChartViewOption.Depth" />
     <PartialsTradeChartInfo v-else />
