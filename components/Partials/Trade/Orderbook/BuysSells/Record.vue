2a2a744008dad6e03db8e41d0fcea566d700d18e
thomasRalee <thomas.leera@gmail.com>
2025-02-27 01:29:23 +0800

    fix: clicking orderbook price value on limit price results in a missing digit in btc perp - PS-92

--- components/Partials/Trade/Orderbook/BuysSells/Record.vue
+++ components/Partials/Trade/Orderbook/BuysSells/Record.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import {
   BigNumber,
-  BigNumberInBase,
-  BigNumberInWei
+  BigNumberInWei,
+  BigNumberInBase
 } from '@injectivelabs/utils'
 import { OrderbookFormattedRecord } from '@/types/worker'
 import {
@@ -17,14 +17,12 @@ const props = withDefaults(
   defineProps<{
     isBuy?: boolean
     index?: number
-    record: OrderbookFormattedRecord
     isActive?: boolean
     highestVolume: string
+    record: OrderbookFormattedRecord
   }>(),
   {
-    isBuy: false,
-    index: -1,
-    isActive: false
+    index: -1
   }
 )
 
@@ -164,7 +162,14 @@ function setQuantityFlashOff() {
 }
 
 function handlePriceClick() {
-  useEventBus(BusEvents.OrderbookPriceClick).emit(props.record.price)
+  const formattedPrice =
+    aggregation.value < 0
+      ? new BigNumberInBase(props.record.price)
+          .times(new BigNumberInBase(10).exponentiatedBy(-aggregation.value))
+          .toFixed()
+      : props.record.price
+
+  useEventBus(BusEvents.OrderbookPriceClick).emit(formattedPrice)
 }
 </script>
 
