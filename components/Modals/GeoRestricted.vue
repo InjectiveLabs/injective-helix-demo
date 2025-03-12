5f4388a34c5fa9b695ef94f64725e59a3313f66a
thomasRalee <thomas.leera@gmail.com>
2025-03-12 18:08:43 +0800

    chore: update geo restriction modal copy

--- components/Modals/GeoRestricted.vue
+++ components/Modals/GeoRestricted.vue
@@ -5,9 +5,10 @@ const modalStore = useSharedModalStore()
 </script>
 
 <template>
-  <AppModal v-model="modalStore.modals[Modal.GeoRestricted]">
-    <div class="py-8">
-      <p class="font-bold text-md text-center">
+  <AppModal v-model="modalStore.modals[Modal.GeoRestricted]" is-md>
+    <div class="py-8 text-center">
+      <h2 class="font-bold text-lg">{{ $t('geoRestricted.title') }}</h2>
+      <p class="text-sm mt-4">
         {{ $t('geoRestricted.description') }}
       </p>
     </div>
