a8732deedce90e8313df8568d587691776cb76bd
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2025-03-21 13:27:19 +0100

    chore: add testnet faucet

--- app/services/referral.ts
+++ app/services/referral.ts
@@ -1,8 +1,11 @@
 import { HttpClient } from '@injectivelabs/utils'
+import { IS_TESTNET } from '@shared/utils/constant'
 
 export const fundReferee = async (refereeAddress: string) => {
   // Maybe we can move to env if needed
-  const lambdaApi = 'https://6vomscnow9.execute-api.us-east-1.amazonaws.com/v1'
+  const lambdaApi = IS_TESTNET
+    ? 'https://182lv47gxg.execute-api.us-east-1.amazonaws.com/v1'
+    : 'https://6vomscnow9.execute-api.us-east-1.amazonaws.com/v1'
 
   const TOTAL_RETRIES = 5
   const DELAY_BETWEEN_CALLS = 1000
