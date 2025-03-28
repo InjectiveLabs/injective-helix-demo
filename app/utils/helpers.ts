6bcec3883d1f74ea519d33f41c9b6ea0b4bf3bf8
thomasRalee <thomas.leera@gmail.com>
2025-03-29 07:56:51 +0800

    chore: point chart endpoint to US service

--- app/utils/helpers.ts
+++ app/utils/helpers.ts
@@ -61,8 +61,9 @@ export const getChronosDatafeedEndpoint = (marketType: string): string => {
   // return `https://k8s.mainnet.exchange.grpc-web.injective.network/api/chronos/v1/${marketType}`
 
   if (IS_MAINNET) {
-    return `https://k8s.global.mainnet.chart.grpc-web.injective.network/api/chart/v1/${marketType}`
-    // return `https://k8s.global.mainnet.chronos.grpc-web.injective.network/api/chronos/v1/${marketType}`
+    // [US region] chart service - EU service is temp down
+    return `https://k8s.mainnet.chart.grpc-web.injective.network/api/chart/v1/${marketType}`
+    // return `https://k8s.global.mainnet.chart.grpc-web.injective.network/api/chart/v1/${marketType}`
   }
 
   // if (IS_TESTNET) {
