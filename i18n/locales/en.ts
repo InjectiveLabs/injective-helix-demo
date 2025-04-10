3d9c0d40ac01594c78d70b92902f326a6d323509
Ivan Angelkoski <ivanangelkoski95@gmail.com>
2025-04-10 21:39:07 +0200

    fix: locales

--- i18n/locales/en.ts
+++ i18n/locales/en.ts
@@ -477,5 +477,10 @@ export default {
     subscribeDescription: 'Get real-time notifications via Notifi',
     footer:
       "We're making Helix better. We appreciate your patience and will be back soon!"
+  },
+
+  footer: {
+    operational: 'Operational',
+    v2: ' Version 2.0'
   }
 }
