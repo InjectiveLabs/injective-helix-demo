993cc476459988dd9e31bc0875767521bbcc60df
thomasRalee <thomas.leera@gmail.com>
2025-04-17 19:15:42 +0800

    chore: remove static routes generation

--- nuxt-config/hooks/index.ts
+++ nuxt-config/hooks/index.ts
@@ -1,13 +1,5 @@
 import { TradePage, TradeSubPage } from './../../types/page'
-import {
-  expiryMarketIdMap,
-  verifiedSpotMarketIdMap,
-  verifiedDerivateMarketIdMap
-} from './../../app/json'
-import type { NitroConfig } from 'nitropack'
 import type { NuxtHooks } from 'nuxt/schema'
-
-const customStaticRoutes: string[] = []
 
 export default {
   'pages:extend'(pages) {
@@ -29,28 +21,5 @@ export default {
         name: TradeSubPage.Spot
       })
     }
-  },
-  'nitro:config'(nitroConfig: NitroConfig) {
-    if (
-      nitroConfig.dev ||
-      !nitroConfig.prerender ||
-      !nitroConfig.prerender.routes
-    ) {
-      return
-    }
-
-    nitroConfig.prerender.routes = [
-      ...nitroConfig.prerender.routes,
-      ...customStaticRoutes,
-      ...Object.keys(verifiedSpotMarketIdMap).map((s) => `/spot/${s}`),
-      ...Object.values(verifiedSpotMarketIdMap).map((s) => `/spot/${s}`),
-      ...[
-        ...Object.keys(verifiedDerivateMarketIdMap),
-        ...Object.keys(expiryMarketIdMap)
-      ].map((s) => `/futures/${s}`),
-      ...['ef3bc2', '25269b', '5f90cb', '50be68'].map(
-        (guildId) => `/guild/${guildId}`
-      )
-    ]
   }
 } as NuxtHooks
