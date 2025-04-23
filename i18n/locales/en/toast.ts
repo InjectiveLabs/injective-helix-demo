c82c819d8c7b8865183ac77da7f148fdf559b361
frederick-88 <frederickfd88@gmail.com>
2025-04-24 03:45:23 +0700

    chore: minor sort

--- i18n/locales/en/toast.ts
+++ i18n/locales/en/toast.ts
@@ -2,9 +2,9 @@ export default {
   toast: {
     error: 'Error',
     success: 'Success',
-    successfullyConnected: 'Successfully Connected',
     addressCopied: 'Address Copied',
     somethingHappened: 'Something Happened...',
+    successfullyConnected: 'Successfully Connected',
     formSubmittedSuccesfully: 'Form Submitted Successfully.',
     copiedAddressToClipboard: 'Address copied to your clipboard',
     subscribeToast: "You've successfully signed up for Helix notifications!",
@@ -18,20 +18,20 @@ export default {
       bridgeNow: 'Bridge Now',
       buyCrypto: 'Buy crypto',
       buyCryptoInstantly: 'Buy crypto instantly',
+      bankTransferSuccessful: 'Transfer Successful',
       startTradingInSeconds: 'Start Trading in Seconds!',
       readyToTrade: 'Ready to Trade? Explore Markets Now!',
       moveAssetsToInj: 'Move assets from Ethereum to Injective',
       discoverTrendingPairs: 'Discover trending pairs and start trading',
       moveAssetsToInjTitle: 'Move Your Crypto to Injective & Start Trading',
-      bankTransferSuccessful: 'Transfer Successful',
       autoSign: {
+        disabledToast: 'Auto sign is disabled',
         allowsYouToTrade:
           'Allows you to trade on Helix for 72 hours without needing to sign most transactions. Auto-Sign can be deactivated any time.',
         enabledToast: {
           title: 'Auto sign is enabled',
           description: 'Auto sign is active for 3 days.'
-        },
-        disabledToast: 'Auto sign is disabled'
+        }
       }
     },
     trade: {
@@ -62,9 +62,9 @@ export default {
       }
     },
     referral: {
-      success: 'Your code is successfully claimed. Explore Markets Now!',
-      referralLinkIsUnavailable: 'Referral Link is Unavailable',
       referralLinkCopied: 'Referral Link Copied',
+      referralLinkIsUnavailable: 'Referral Link is Unavailable',
+      success: 'Your code is successfully claimed. Explore Markets Now!',
       joinSelfReferralMessage:
         "Your referral code is valid, but you can't refer yourself. Share it with someone else!"
     },
@@ -73,13 +73,13 @@ export default {
         "We've received your information. Our team will reach out to you shortly."
     },
     campaign: {
+      notFound: 'Campaign not found, please try again later.',
       successfullyClaimedRewards: 'Succesfuly Claimed Rewards',
-      errorAlreadyClaimed: 'This reward has already been claimed.',
-      notFound: 'Campaign not found, please try again later.'
+      errorAlreadyClaimed: 'This reward has already been claimed.'
     },
     guild: {
-      successfullyCreateGuild: 'Successfully created guild',
       successfullyJoinGuild: 'Successfully joined guild',
+      successfullyCreateGuild: 'Successfully created guild',
       copiedInvitationLink: 'Invitation code copied to your clipboard.'
     },
     sgt: {
