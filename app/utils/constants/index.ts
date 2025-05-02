f10ffc0bbf7dd141b977f4c3d29d1e427fed3c7f
billyjacoby <billyjacoby@gmail.com>
2025-05-02 10:45:44 +0200

    chore: update helix mobile connect endpoint

--- app/utils/constants/index.ts
+++ app/utils/constants/index.ts
@@ -185,8 +185,8 @@ export const STOP_REASON_MAP = {
   [StopReason.Emergency]: 'sgt.marketConditionsNotSupported'
 }
 
-export const CONNECT_SERVER_URL =
-  'https://k8s.mainnet.eu.helix-server.injective.network/api'
+export const HELIX_MOBILE_CONNECT_SERVER_URL = import.meta.env
+  .VITE_HELIX_MOBILE_CONNECT_SERVER_URL
 
 export const LIGHT_CHART_MARKET_IDS = [
   '0xe5bfc48fc29146d756c9dac69f096d56cc4fc5ae75c98c1ad045c3356d14eb82' // AIX/USDT PERP
