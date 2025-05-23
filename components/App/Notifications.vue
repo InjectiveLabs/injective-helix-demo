f9236ffd0eede8bd39289f97380dbe5b5ed9f397
frederick-88 <frederickfd88@gmail.com>
2025-05-24 02:43:11 +0700

    chore: integrate event emitter system from wallet-strategy (IL-984)

--- components/App/Notifications.vue
+++ components/App/Notifications.vue
@@ -1,29 +1,37 @@
 <script lang="ts" setup>
+import { walletStrategy } from '@shared/WalletService'
 import { MAX_TOAST_TIMEOUT } from '@shared/utils/constant'
-import { SdkEventEmitter } from '@injectivelabs/wallet-core'
 import { CtaToast } from '@/types'
 
 const notificationStore = useSharedNotificationStore()
 const { t } = useLang()
 
 onMounted(() => {
-  SdkEventEmitter.getInstance().on('transaction-signed', (data) => {
-    if (data?.isTelemetry) {
-      notificationStore.info({
-        isTelemetry: true,
-        key: CtaToast.Telemetry,
-        timeout: MAX_TOAST_TIMEOUT,
-        title: t('toast.broacastingTransaction')
-      })
-    }
-  })
-
-  SdkEventEmitter.getInstance().on('transaction-fail', (data) => {
-    if (data?.isTelemetry) {
-      notificationStore.close(CtaToast.Telemetry)
-    }
-  })
+  walletStrategy.on('transaction-signed', onTransactionSigned)
+  walletStrategy.on('transaction-fail', onTransactionFail)
 })
+
+onUnmounted(() => {
+  walletStrategy.off('transaction-signed', onTransactionSigned)
+  walletStrategy.off('transaction-fail', onTransactionFail)
+})
+
+function onTransactionSigned(data?: Record<string, any>) {
+  if (data?.isTelemetry) {
+    notificationStore.info({
+      isTelemetry: true,
+      key: CtaToast.Telemetry,
+      timeout: MAX_TOAST_TIMEOUT,
+      title: t('toast.broacastingTransaction')
+    })
+  }
+}
+
+function onTransactionFail(data?: Record<string, any>) {
+  if (data?.isTelemetry) {
+    notificationStore.close(CtaToast.Telemetry)
+  }
+}
 </script>
 
 <template>
