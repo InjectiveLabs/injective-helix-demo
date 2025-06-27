bdebc14657ffb0fbb36c2e9594a627599446299a
frederick-88 <frederickfd88@gmail.com>
2025-06-27 21:50:26 +0700

    chore: update as per PR feedback (IL-1567)

--- components/Partials/Portfolio/Portfolio/KeyStats.vue
+++ components/Partials/Portfolio/Portfolio/KeyStats.vue
@@ -3,7 +3,6 @@ import { BigNumberInBase } from '@injectivelabs/utils'
 
 const appStore = useAppStore()
 const accountStore = useAccountStore()
-const { t } = useLang()
 const {
   stakedAmountInUsd,
   aggregatedSubaccountTotalBalanceInUsd,
@@ -18,43 +17,13 @@ const { valueToBigNumber: spotEquityInBigNumber } = useSharedBigNumberFormatter(
   )
 )
 
-const keyStats = computed(() => {
-  const allTimePnl = new BigNumberInBase(accountStore.accountStats?.pnl || 0)
-  const pnlTextColor = allTimePnl.isZero()
-    ? 'text-gray-200'
-    : allTimePnl.gt(0)
-      ? 'text-green-500'
-      : 'text-red-500'
+const totalVolume = computed(
+  () => new BigNumberInBase(accountStore.accountStats?.volume || 0)
+)
 
-  return [
-    {
-      value: allTimePnl,
-      textColor: pnlTextColor,
-      title: t('portfolio.keyStats.allTimePnl')
-    },
-    {
-      title: t('portfolio.keyStats.totalVolume'),
-      value: new BigNumberInBase(accountStore.accountStats?.volume || 0)
-    },
-    {
-      value: aggregatedSubaccountTotalBalanceInUsd.value,
-      title: t('portfolio.keyStats.totalEquity')
-    },
-    {
-      value: aggregatedSubaccountUnrealizedPnlInUsd.value,
-      title: t('portfolio.keyStats.perpsAccountEquity')
-    },
-    {
-      value: spotEquityInBigNumber.value,
-      title: t('portfolio.keyStats.spotAccountEquity')
-    },
-    {
-      isStakingAmount: true,
-      value: stakedAmountInUsd.value,
-      title: t('portfolio.keyStats.stakingAccount')
-    }
-  ]
-})
+const allTimePnl = computed(
+  () => new BigNumberInBase(accountStore.accountStats?.pnl || 0)
+)
 </script>
 
 <template>
@@ -64,19 +33,21 @@ const keyStats = computed(() => {
     </h4>
 
     <ul class="flex flex-col gap-6 mt-6">
-      <li
-        v-for="(stat, index) in keyStats"
-        :key="index"
-        class="flex justify-between gap-4 text-sm h-6"
-      >
-        <span class="text-coolGray-375">{{ stat.title }}</span>
+      <li class="flex justify-between gap-4 text-sm h-6">
+        <span class="text-coolGray-375">
+          {{ $t('portfolio.keyStats.allTimePnl') }}
+        </span>
 
         <p
           class="flex"
           :class="[
             appStore.userState.preferences.isHideBalances
               ? 'text-gray-200'
-              : stat.textColor || 'text-gray-200'
+              : allTimePnl.isZero()
+                ? 'text-gray-200'
+                : allTimePnl.gt(0)
+                  ? 'text-green-500'
+                  : 'text-red-500'
           ]"
         >
           <span class="mt-0.5 text-[13px]">$</span>
@@ -84,7 +55,97 @@ const keyStats = computed(() => {
             <CommonNumberCounter
               v-bind="{
                 size: 13,
-                value: stat.value.toNumber()
+                value: allTimePnl.toNumber()
+              }"
+            />
+          </CommonSkeletonSubaccountAmount>
+        </p>
+      </li>
+
+      <li class="flex justify-between gap-4 text-sm h-6">
+        <span class="text-coolGray-375">
+          {{ $t('portfolio.keyStats.totalVolume') }}
+        </span>
+
+        <p class="flex text-gray-200">
+          <span class="mt-0.5 text-[13px]">$</span>
+          <CommonSkeletonSubaccountAmount>
+            <CommonNumberCounter
+              v-bind="{
+                size: 13,
+                value: totalVolume.toNumber()
+              }"
+            />
+          </CommonSkeletonSubaccountAmount>
+        </p>
+      </li>
+
+      <li class="flex justify-between gap-4 text-sm h-6">
+        <span class="text-coolGray-375">
+          {{ $t('portfolio.keyStats.totalEquity') }}
+        </span>
+
+        <p class="flex text-gray-200">
+          <span class="mt-0.5 text-[13px]">$</span>
+          <CommonSkeletonSubaccountAmount>
+            <CommonNumberCounter
+              v-bind="{
+                size: 13,
+                value: aggregatedSubaccountTotalBalanceInUsd.toNumber()
+              }"
+            />
+          </CommonSkeletonSubaccountAmount>
+        </p>
+      </li>
+
+      <li class="flex justify-between gap-4 text-sm h-6">
+        <span class="text-coolGray-375">
+          {{ $t('portfolio.keyStats.perpsAccountEquity') }}
+        </span>
+
+        <p class="flex text-gray-200">
+          <span class="mt-0.5 text-[13px]">$</span>
+          <CommonSkeletonSubaccountAmount>
+            <CommonNumberCounter
+              v-bind="{
+                size: 13,
+                value: aggregatedSubaccountUnrealizedPnlInUsd.toNumber()
+              }"
+            />
+          </CommonSkeletonSubaccountAmount>
+        </p>
+      </li>
+
+      <li class="flex justify-between gap-4 text-sm h-6">
+        <span class="text-coolGray-375">
+          {{ $t('portfolio.keyStats.spotAccountEquity') }}
+        </span>
+
+        <p class="flex text-gray-200">
+          <span class="mt-0.5 text-[13px]">$</span>
+          <CommonSkeletonSubaccountAmount>
+            <CommonNumberCounter
+              v-bind="{
+                size: 13,
+                value: spotEquityInBigNumber.toNumber()
+              }"
+            />
+          </CommonSkeletonSubaccountAmount>
+        </p>
+      </li>
+
+      <li class="flex justify-between gap-4 text-sm h-6">
+        <span class="text-coolGray-375">
+          {{ $t('portfolio.keyStats.stakingAccount') }}
+        </span>
+
+        <p class="flex text-gray-200">
+          <span class="mt-0.5 text-[13px]">$</span>
+          <CommonSkeletonSubaccountAmount>
+            <CommonNumberCounter
+              v-bind="{
+                size: 13,
+                value: stakedAmountInUsd.toNumber()
               }"
             />
           </CommonSkeletonSubaccountAmount>
