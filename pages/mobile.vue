bf3156e1a5611a90fb6f423b0a3d45bf5d3f55e5
billyjacoby <billyjacoby@gmail.com>
2025-07-09 11:08:40 -0400

    feat: add mobile link for device specific download

---
+++ pages/mobile.vue
@@ -0,0 +1,47 @@
+<script lang="ts" setup>
+import { MainPage } from '@/types'
+// https://apps.apple.com/us/app/helix-trade-with-control/id6737353178?itscg=30200&itsct=apps_box_link&mttnsubad=6737353178
+// https://play.google.com/store/apps/details?id=com.injectivelabs.helixmobile
+
+const mobileOperatingSystem = ref('')
+
+function getMobileOperatingSystem() {
+  // @ts-expect-error - might not actually exist
+  const userAgent = navigator.userAgent || navigator?.vendor || window?.opera
+
+  if (/android/i.test(userAgent)) {
+    return 'Android'
+  }
+
+  // iOS detection from: http://stackoverflow.com/a/9039885/177710
+  // @ts-expect-error - might not exist
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window?.MSStream) {
+    return 'iOS'
+  }
+
+  return 'unknown'
+}
+
+onMounted(() => {
+  mobileOperatingSystem.value = getMobileOperatingSystem()
+
+  if (mobileOperatingSystem.value === 'iOS') {
+    return navigateTo(
+      'https://apps.apple.com/us/app/helix-trade-with-control/id6737353178?itscg=30200&itsct=apps_box_link&mttnsubad=6737353178',
+      { external: true }
+    )
+  }
+  if (mobileOperatingSystem.value === 'Android') {
+    return navigateTo(
+      'https://play.google.com/store/apps/details?id=com.injectivelabs.helixmobile',
+      { external: true }
+    )
+  }
+
+  return navigateTo({ name: MainPage.Index })
+})
+</script>
+
+<template>
+  <div />
+</template>
