3d7c3f52b4491e4ecd90742382c2c44f36cc92b0
thomasRalee <thomas.leera@gmail.com>
2025-07-29 22:25:00 +0800

    chore: add support to hardcode tokens for devnet testing

--- app/data/token.ts
+++ app/data/token.ts
@@ -1,3 +1,5 @@
+import { unknownToken } from '@shared/data/token'
+
 export enum USDCSymbol {
   PeggyEthereum = 'USDC',
   WormholeEthereum = 'USDCet',
@@ -26,3 +28,33 @@ export const KAVA_USDT_SYMBOL = 'USDTkv'
 export const STINJ_USDT_SYMBOL = 'STINJ'
 export const allowanceResetSymbols = ['USDT']
 export const SWAP_LOW_LIQUIDITY_SYMBOLS = ['GF', 'ORAI', 'SOMM', 'NEOK']
+
+// for testing only
+export const hardCodedDevnetTokens = [
+  {
+    address: 'kava',
+    isNative: false,
+    tokenVerification: 'verified',
+    decimals: 6,
+    symbol: 'KAVA',
+    name: 'KAVA',
+    logo: unknownToken.logo,
+    coinGeckoId: '',
+    tokenType: 'symbol',
+    denom: 'kava',
+    externalLogo: 'unknown.png'
+  },
+  {
+    address: 'uni',
+    isNative: false,
+    tokenVerification: 'verified',
+    decimals: 6,
+    symbol: 'UNI',
+    name: 'UNI',
+    logo: unknownToken.logo,
+    coinGeckoId: '',
+    tokenType: 'symbol',
+    denom: 'uni',
+    externalLogo: 'unknown.png'
+  }
+]
