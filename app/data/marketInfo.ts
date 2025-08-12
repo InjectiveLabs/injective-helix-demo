e2b36864ffb156a0ef9ba9cb620b9530a70d31da
0xA1337 <0xa1337@proton.me>
2025-08-12 15:15:05 +0200

    chore: add todo comment

--- app/data/marketInfo.ts
+++ app/data/marketInfo.ts
@@ -223,6 +223,7 @@ export const MARKETS_POWERED_BY_STORK = [
 ]
 
 export const MARKETS_POWERED_BY_SQUARETOWER = [
+  // TODO: Update market ID when it's live
   '0xc4068b76801bf8988b5372c64d611ce2a477f85512e5ec55270c426176bc73e2'
 ]
 
