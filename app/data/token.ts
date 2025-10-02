1c01d722194f077fb4925fa32433541db5b568c0
thomasRalee <thomas.leera@gmail.com>
2025-10-02 19:21:18 +0800

    chore: add devnet aave token for testing

--- app/data/token.ts
+++ app/data/token.ts
@@ -54,5 +54,19 @@ export const hardCodedDevnetTokens = [
     tokenType: 'symbol',
     denom: 'uni',
     externalLogo: 'unknown.png'
+  },
+  {
+    address: 'factory/inj17gkuet8f6pssxd8nycm3qr9d9y699rupv6397z/AAVE',
+    isNative: false,
+    tokenVerification: 'verified',
+    decimals: 8,
+    name: 'AAVE',
+    symbol: 'AAVE',
+    coinGeckoId: 'injective-quants',
+    logo: 'https://imagedelivery.net/lPzngbR8EltRfBOi_WYaXw/6f015260-c589-499f-b692-a57964af9900/public',
+    creator: 'inj127l5a2wmkyvucxdlupqyac3y0v6wqfhq03ka64',
+    denom: 'factory/inj17gkuet8f6pssxd8nycm3qr9d9y699rupv6397z/AAVE',
+    tokenType: 'tokenFactory',
+    externalLogo: 'qunt.webp'
   }
 ]
