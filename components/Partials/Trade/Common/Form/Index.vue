7daa0eff3ddedf633dc871ce5bd77631af4a1557
0xA1337 <arthur@injectivelabs.org>
2025-11-05 20:12:17 +0100

    refactor: turn root form component into unified component

--- components/Partials/Trade/Common/Form/Index.vue
+++ components/Partials/Trade/Common/Form/Index.vue
@@ -1,20 +1,21 @@
 <script setup lang="ts">
 import { dataCyTag } from '@shared/utils'
 import { NuxtUiIcons } from '@shared/types'
-import {
-  MarketKey,
-  UiSpotMarket,
-  TradingInterface,
-  SpotMarketCyTags
-} from '@/types'
+import { TradingInterface, SpotMarketCyTags, UiMarketWithToken } from '@/types'
 
 const jsonStore = useSharedJsonStore()
 
-const spotMarket = inject(MarketKey) as Ref<UiSpotMarket>
+const props = withDefaults(
+  defineProps<{
+    isSpot?: boolean
+    market: UiMarketWithToken
+  }>(),
+  {}
+)
 
 const queryTradingMode = useQueryRef('interface', TradingInterface.Standard)
 
-const isReady = ref(false)
+const isMounted = ref(false)
 
 const options = computed(() => [
   {
@@ -25,15 +26,18 @@ const options = computed(() => [
   {
     icon: NuxtUiIcons.Robot2,
     value: TradingInterface.TradingBots,
-    disabled:
-      jsonStore.spotGridMarkets.find(
-        ({ slug }) => slug === spotMarket.value.slug
-      ) === undefined
+    disabled: props.isSpot
+      ? jsonStore.spotGridMarkets.find(
+          ({ slug }) => slug === props.market.slug
+        ) === undefined
+      : jsonStore.derivativeGridMarkets.find(
+          ({ slug }) => slug === props.market.slug
+        ) === undefined
   }
 ])
 
 onMounted(() => {
-  isReady.value = true
+  isMounted.value = true
 
   if (
     queryTradingMode.value === TradingInterface.TradingBots &&
@@ -49,15 +53,17 @@ onMounted(() => {
   <div>
     <div
       class="h-subHeader flex border-b"
-      :data-cy="dataCyTag(SpotMarketCyTags.SpotTradingMode)"
+      :data-cy="
+        isSpot ? dataCyTag(SpotMarketCyTags.SpotTradingMode) : undefined
+      "
     >
       <AppButtonSelect
         v-for="{ icon, value, disabled } in options"
         :key="value"
         v-model="queryTradingMode"
         v-bind="{ value, disabled }"
-        class="font-bold text-sm flex flex-col gap-1 justify-center items-center px-6 border-r last:border-r-0 text-coolGray-450 flex-1"
         active-classes="bg-brand-875 text-white"
+        class="font-bold text-sm flex flex-col gap-1 justify-center items-center px-6 border-r last:border-r-0 text-coolGray-450 flex-1"
       >
         <UIcon :name="icon" class="size-5 min-w-5" />
         <span class="leading-none">{{ $t(`trade.${value}`) }}</span>
@@ -65,11 +71,12 @@ onMounted(() => {
     </div>
 
     <div>
-      <PartialsTradeSpotFormStandard
-        v-if="isReady && queryTradingMode === TradingInterface.Standard"
+      <slot
+        v-if="isMounted && queryTradingMode === TradingInterface.Standard"
+        name="standard"
       />
 
-      <PartialsTradeSpotFormTradingBots v-else />
+      <slot v-else name="bots" />
     </div>
   </div>
 </template>
